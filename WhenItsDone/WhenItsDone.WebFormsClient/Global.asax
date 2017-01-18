bd71729d7a0fb27437d8b7b09cec4453c6b87838
shakuu <todorov.h@gmail.com>
2017-01-18 11:05:51 +0200

    initial

---
+++ WhenItsDone/WhenItsDone.WebFormsClient/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WhenItsDone.WebFormsClient.Global" Language="C#" %>
---
+++ WhenItsDone/WhenItsDone.WebFormsClient/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WhenItsDone.WebFormsClient.Startup))]
+namespace WhenItsDone.WebFormsClient
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
