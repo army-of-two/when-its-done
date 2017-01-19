34b74c66f69ed5ac4caa25bfd764d262ac1f9831
shakuu <todorov.h@gmail.com>
2017-01-19 02:42:44 +0200

    refactoring ninject module

--- WhenItsDone/Clients/WhenItsDone.WebFormsClient/App_Start/NinjectBindingsModules/MVPBindingsModule.cs
+++ WhenItsDone/Clients/WhenItsDone.WebFormsClient/App_Start/NinjectBindingsModules/MVPBindingsModule.cs
@@ -9,6 +9,7 @@ using WebFormsMvp;
 using WebFormsMvp.Binder;
 
 using WhenItsDone.WebFormsClient.App_Start.Factories;
+using Ninject.Activation;
 
 namespace WhenItsDone.WebFormsClient.App_Start.NinjectBindingsModules
 {
@@ -19,43 +20,45 @@ namespace WhenItsDone.WebFormsClient.App_Start.NinjectBindingsModules
             this.Bind<IPresenterFactory>().To<CustomWebFormsMvpPresenterFactory>().InSingletonScope();
             this.Bind<ICustomPresenterFactory>().ToFactory().InSingletonScope();
 
-            this.Bind<IPresenter>().ToMethod(ctx =>
+            this.Bind<IPresenter>().ToMethod(this.PresenterFactoryMethod)
+                .NamedLikeFactoryMethod((ICustomPresenterFactory factory) => factory.GetPresenter(null, null, null));
+        }
+
+        private IPresenter PresenterFactoryMethod(IContext ctx)
+        {
+            var parameters = ctx.Parameters.ToList();
+
+            var requestedType = (Type)parameters[0];
+            if (requestedType == null)
             {
-                var parameters = ctx.Parameters.ToList();
+                throw new ArgumentNullException("Invalid requested presenter type.");
+            }
 
-                var requestedType = (Type)parameters[0];
-                if (requestedType == null)
-                {
-                    throw new ArgumentNullException("Invalid requested presenter type.");
-                }
+            var viewType = (Type)parameters[1];
+            if (viewType == null)
+            {
+                throw new ArgumentNullException("Invalid requested view type.");
+            }
 
-                var viewType = (Type)parameters[1];
-                if (viewType == null)
-                {
-                    throw new ArgumentNullException("Invalid requested view type.");
-                }
+            var viewInstance = (IView)parameters[2];
+            if (viewInstance == null)
+            {
+                viewInstance = (IView)ctx.Kernel.Get(viewType);
+            }
 
-                var viewInstance = (IView)parameters[2];
-                if (viewInstance == null)
-                {
-                    viewInstance = (IView)ctx.Kernel.Get(viewType);
-                }
+            // Unknown possible parameters for each separate IPresenter
+            // Binding the view so Ninject can resolve each of them separately.
+            var bindingExists = this.Kernel.GetBindings(viewType).Any();
+            if (bindingExists)
+            {
+                this.Rebind(viewType).ToMethod(context => viewInstance);
+            }
+            else
+            {
+                this.Bind(viewType).ToMethod(context => viewInstance);
+            }
 
-                // Unknown possible parameters for each separate IPresenter
-                // Binding the view so Ninject can resolve each of them separately.
-                var bindingExists = this.Kernel.GetBindings(viewType).Any();
-                if (bindingExists)
-                {
-                    this.Rebind(viewType).ToMethod(context => viewInstance);
-                }
-                else
-                {
-                    this.Bind(viewType).ToMethod(context => viewInstance);
-                }
-
-                return (IPresenter)ctx.Kernel.Get(requestedType);
-            })
-            .NamedLikeFactoryMethod((ICustomPresenterFactory factory) => factory.GetPresenter(null, null, null));
+            return (IPresenter)ctx.Kernel.Get(requestedType);
         }
     }
 }
