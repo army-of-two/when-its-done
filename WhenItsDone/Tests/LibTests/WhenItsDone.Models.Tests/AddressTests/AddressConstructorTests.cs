854bda79b9e1622302aa81376cfd7e8120bbcd00
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-20 17:21:28 +0200

    reordering address constructor test and add more

--- WhenItsDone/Tests/LibTests/WhenItsDone.Models.Tests/AddressTests/AddressConstructorTests.cs
+++ WhenItsDone/Tests/LibTests/WhenItsDone.Models.Tests/AddressTests/AddressConstructorTests.cs
@@ -12,5 +12,45 @@ namespace WhenItsDone.Models.Tests.AddressTests
 
             Assert.IsInstanceOf<Address>(obj);
         }
+
+        [Test]
+        public void Constructor_ShouldNotSet_IdProperty()
+        {
+            var obj = new Address();
+
+            Assert.AreEqual(0, obj.Id);
+        }
+
+        [Test]
+        public void Constructor_ShouldNotSet_CountryProperty()
+        {
+            var obj = new Address();
+
+            Assert.AreEqual(null, obj.Country);
+        }
+
+        [Test]
+        public void Constructor_ShouldNotSet_CityProperty()
+        {
+            var obj = new Address();
+
+            Assert.AreEqual(null, obj.City);
+        }
+
+        [Test]
+        public void Constructor_ShouldNotSet_StreetProperty()
+        {
+            var obj = new Address();
+
+            Assert.AreEqual(null, obj.Street);
+        }
+
+        [Test]
+        public void Constructor_ShouldNotSet_IsDeletedProperty()
+        {
+            var obj = new Address();
+
+            Assert.IsFalse(obj.IsDeleted);
+        }
     }
 }
