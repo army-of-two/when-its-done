d1f1005b16e1149e44fe6713a29da0ae5aac1462
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-20 15:59:55 +0200

    added Address constructor tests

---
+++ WhenItsDone/Tests/LibTests/WhenItsDone.Models.Tests/AddressTests/AddressConstructorTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace WhenItsDone.Models.Tests.AddressTests
+{
+    [TestFixture]
+    public class AddressConstructorTests
+    {
+        [Test]
+        public void Address_ShouldHave_ParameterlessConstructor()
+        {
+            var obj = new Address();
+
+            Assert.IsInstanceOf<Address>(obj);
+        }
+    }
+}
