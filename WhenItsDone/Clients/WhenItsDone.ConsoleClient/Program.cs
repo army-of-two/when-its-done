175a53a6e951530671f401d791c35dc7d887c992
shakuu <todorov.h@gmail.com>
2017-01-21 01:35:08 +0200

    testing app

---
+++ WhenItsDone/Clients/WhenItsDone.ConsoleClient/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhenItsDone.ConsoleClient
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Program.AsyncTest();
+            var result = Program.SyncTest();
+            Console.WriteLine(result.Result);
+        }
+
+        private static void AsyncTest()
+        {
+            var counter = 0;
+            Task.Run(() =>
+            {
+                for (long i = 0; i < 999999999999; i++)
+                {
+                    counter++;
+                }
+            });
+
+            Console.WriteLine(counter);
+        }
+
+        private async static Task<long> SyncTest()
+        {
+            var counter = 0;
+            return await Task.Run(() =>
+             {
+                 for (long i = 0; i < 999999999999; i++)
+                 {
+                     counter++;
+                 }
+
+                 return counter;
+             });
+        }
+    }
+}
