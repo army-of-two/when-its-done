7bc7337fe012e55f7745e4d7f6c9517313df8db6
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-20 19:13:42 +0200

    added ClientReview as DbModel tests

---
+++ WhenItsDone/Tests/LibTests/WhenItsDone.Models.Tests/ClientReviewTests/ClientReviewAsDbModel.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System.Linq;
+using WhenItsDone.Models.Contracts;
+
+namespace WhenItsDone.Models.Tests.ClientReviewTests
+{
+    [TestFixture]
+    public class ClientReviewAsDbModel
+    {
+        [Test]
+        public void ClientReviewClass_ShouldImplement_IDbModelInterface()
+        {
+            var obj = new ClientReview();
+
+            var result = obj.GetType()
+                            .GetInterfaces()
+                            .Where(x => x == typeof(IDbModel))
+                            .Any();
+
+            Assert.IsTrue(result);
+        }
+    }
+}
