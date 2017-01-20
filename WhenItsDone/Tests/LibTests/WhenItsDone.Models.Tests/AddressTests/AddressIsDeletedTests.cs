da309692a973ab11ea6a7d24b03e38437db60990
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-20 16:09:15 +0200

    added Address.IsDeleted tests

---
+++ WhenItsDone/Tests/LibTests/WhenItsDone.Models.Tests/AddressTests/AddressIsDeletedTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace WhenItsDone.Models.Tests.AddressTests
+{
+    [TestFixture]
+    public class AddressIsDeletedTests
+    {
+        [Test]
+        public void IsDeleted_InitialValue_ShouldBeFalse()
+        {
+            var obj = new Address();
+
+            Assert.IsFalse(obj.IsDeleted);
+        }
+    }
+}
