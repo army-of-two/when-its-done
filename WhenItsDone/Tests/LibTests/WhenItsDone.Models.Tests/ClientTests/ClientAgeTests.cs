6cf399f5766c5bcc8264dbeafcff7dd3e2d8fe1e
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-20 17:44:47 +0200

    added Client.Age tests

---
+++ WhenItsDone/Tests/LibTests/WhenItsDone.Models.Tests/ClientTests/ClientAgeTests.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using System.Linq;
+using WhenItsDone.Models.Constants;
+
+namespace WhenItsDone.Models.Tests.ClientTests
+{
+    [TestFixture]
+    public class ClientAgeTests
+    {
+        [Test]
+        public void Age_ShouldHave_RangeAttribute()
+        {
+            var obj = new Client();
+
+            var result = obj.GetType()
+                            .GetProperty("Age")
+                            .GetCustomAttributes(false)
+                            .Where(x => x.GetType() == typeof(System.ComponentModel.DataAnnotations.RangeAttribute))
+                            .Any();
+
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void Age_ShouldHave_RightMinValueFor_RangeAttribute()
+        {
+            var obj = new Client();
+
+            var result = obj.GetType()
+                            .GetProperty("Age")
+                            .GetCustomAttributes(false)
+                            .Where(x => x.GetType() == typeof(System.ComponentModel.DataAnnotations.RangeAttribute))
+                            .Select(x => (System.ComponentModel.DataAnnotations.RangeAttribute)x)
+                            .SingleOrDefault();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(ValidationConstants.AgeMinValue, result.Minimum);
+        }
+
+        [Test]
+        public void Age_ShouldHave_RightMaxValueFor_RangeAttribute()
+        {
+            var obj = new Client();
+
+            var result = obj.GetType()
+                            .GetProperty("Age")
+                            .GetCustomAttributes(false)
+                            .Where(x => x.GetType() == typeof(System.ComponentModel.DataAnnotations.RangeAttribute))
+                            .Select(x => (System.ComponentModel.DataAnnotations.RangeAttribute)x)
+                            .SingleOrDefault();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(ValidationConstants.AgeMinValue, result.Maximum);
+        }
+    }
+}
