eceb51e618e7ffe77ec23a41ba321a33d9996118
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-20 17:26:27 +0200

    added Client.Id tests

---
+++ WhenItsDone/Tests/LibTests/WhenItsDone.Models.Tests/ClientTests/ClientIdTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace WhenItsDone.Models.Tests.ClientTests
+{
+    [TestFixture]
+    public class ClientIdTests
+    {
+        [Test]
+        public void Id_ShouldHave_KeyAttribute()
+        {
+            var obj = new Client();
+
+            var result = obj.GetType()
+                            .GetProperty("Id")
+                            .GetCustomAttributes(false)
+                            .Where(x => x.GetType() == typeof(KeyAttribute))
+                            .Any();
+
+            Assert.IsTrue(result);
+        }
+    }
+}
