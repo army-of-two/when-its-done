b5b40a1d5f9d84613bc536c3781df9f50de20ed6
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-20 16:06:16 +0200

    added Address.Street tests

---
+++ WhenItsDone/Tests/LibTests/WhenItsDone.Models.Tests/AddressTests/AddressStreetTests.cs
@@ -0,0 +1,115 @@
+﻿using NUnit.Framework;
+using System.ComponentModel.DataAnnotations;
+using WhenItsDone.Models.Constants;
+using System.Linq;
+
+namespace WhenItsDone.Models.Tests.AddressTests
+{
+    [TestFixture]
+    public class AddressStreetTests
+    {
+        [Test]
+        public void Street_ShouldHave_RequiredAttribute()
+        {
+            var obj = new Address();
+
+            var result = obj.GetType()
+                            .GetProperty("Street")
+                            .GetCustomAttributes(false)
+                            .Where(x => x.GetType() == typeof(RequiredAttribute))
+                            .Any();
+
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void Street_ShouldHave_MinLengthAttribute()
+        {
+            var obj = new Address();
+
+            var result = obj.GetType()
+                            .GetProperty("Street")
+                            .GetCustomAttributes(false)
+                            .Where(x => x.GetType() == typeof(MinLengthAttribute))
+                            .Any();
+
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void Street_ShouldHave_RightValue_InMinLengthAttribute()
+        {
+            var obj = new Address();
+
+            var result = obj.GetType()
+                            .GetProperty("Street")
+                            .GetCustomAttributes(false)
+                            .Where(x => x.GetType() == typeof(MinLengthAttribute))
+                            .Select(x => (MinLengthAttribute)x)
+                            .SingleOrDefault();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(ValidationConstants.StreetMinLength, result.Length);
+        }
+
+        [Test]
+        public void Street_ShouldHave_MaxLengthAttribute()
+        {
+            var obj = new Address();
+
+            var result = obj.GetType()
+                            .GetProperty("Street")
+                            .GetCustomAttributes(false)
+                            .Where(x => x.GetType() == typeof(MaxLengthAttribute))
+                            .Any();
+
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void Street_ShouldHave_RightValue_InMaxLengthAttribute()
+        {
+            var obj = new Address();
+
+            var result = obj.GetType()
+                            .GetProperty("Street")
+                            .GetCustomAttributes(false)
+                            .Where(x => x.GetType() == typeof(MaxLengthAttribute))
+                            .Select(x => (MaxLengthAttribute)x)
+                            .SingleOrDefault();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(ValidationConstants.StreetMaxLength, result.Length);
+        }
+
+        [Test]
+        public void Street_ShouldHave_RegularExpressionAttribute()
+        {
+            var obj = new Address();
+
+            var result = obj.GetType()
+                            .GetProperty("Street")
+                            .GetCustomAttributes(false)
+                            .Where(x => x.GetType() == typeof(RegularExpressionAttribute))
+                            .Any();
+
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void Street_ShouldHave_RightValue_InRegularExpressionAttribute()
+        {
+            var obj = new Address();
+
+            var result = obj.GetType()
+                            .GetProperty("Street")
+                            .GetCustomAttributes(false)
+                            .Where(x => x.GetType() == typeof(RegularExpressionAttribute))
+                            .Select(x => (RegularExpressionAttribute)x)
+                            .SingleOrDefault();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(RegexConstants.EnBgSpaceMinus, result.Pattern);
+        }
+    }
+}
