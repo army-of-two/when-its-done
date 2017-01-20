ddcbb4541569e2a9b8266877e3ab95353e2d68ff
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-20 15:18:54 +0200

    added moq and nunit to all test classes and added 1st test

---
+++ WhenItsDone/Tests/LibTests/WhenItsDone.Models.Tests/AddressTests/AddressAsDbModelTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
+using WhenItsDone.Models;
+using WhenItsDone.Models.Contracts;
+
+namespace WhenItsDone.Models.Tests.AddressTests
+{
+    [TestFixture]
+    public class AddressAsDbModelTests
+    {
+        [Test]
+        public void AddressClass_ShouldImplement_IDbModelInterface()
+        {
+            var obj = new Address();
+
+            Assert.IsTrue(obj.GetType()
+                            .GetInterfaces()
+                            .Any(x=>x==typeof(IDbModel)));
+        }
+    }
+}
