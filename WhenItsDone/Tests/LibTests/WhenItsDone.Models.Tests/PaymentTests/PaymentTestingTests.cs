825e40cd56708e8e73e4787ec40515cdb0229c67
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-21 15:15:51 +0200

    added Payment test of tests

---
+++ WhenItsDone/Tests/LibTests/WhenItsDone.Models.Tests/PaymentTests/PaymentTestingTests.cs
@@ -0,0 +1,335 @@
+﻿using NUnit.Framework;
+using System.Linq;
+
+namespace WhenItsDone.Models.Tests.PaymentTests
+{
+    [TestFixture]
+    public class PaymentTestingTests
+    {
+        /// <summary>
+        /// At that moment Payment class have 5 tested properties .. if another property is added this test will fail
+        /// That mean new tests are REQUIRED!
+        /// </summary>
+        [Test]
+        public void Payment_VerifyNumberOfProperties()
+        {
+            var obj = new Payment();
+
+            var result = obj.GetType()
+                            .GetProperties()
+                            .Count();
+
+            Assert.AreEqual(5, result);
+        }
+
+        /// <summary>
+        /// At that moment Payment class have 1 tested constructor
+        /// </summary>
+        [Test]
+        public void Payment_VerifyNumberOfConstructors()
+        {
+            var obj = new Payment();
+
+            var methodsCount = obj.GetType()
+                                    .GetMethods()
+                                    .Count();
+
+            var propertiesCount = obj.GetType()
+                                    .GetProperties()
+                                    .Count();
+
+            var result = obj.GetType()
+                            .GetMembers()
+                            .Count();
+
+            result = result - propertiesCount - methodsCount;
+
+            Assert.AreEqual(1, result);
+        }
+
+        /// <summary>
+        /// At that moment Payment class have 0 tested methods
+        /// </summary>
+        [Test]
+        public void Payment_VerifyNumberOfMethods()
+        {
+            var methodsFromFramework = 4;
+            var expectedMethodsCount = 0;
+            var totalMethodsCount = methodsFromFramework + expectedMethodsCount;
+
+            var obj = new Payment();
+
+            var numberOfMethodsComeFromProperties = obj.GetType()
+                                                        .GetProperties()
+                                                        .Select(x => 2)
+                                                        .Sum();
+
+            var result = obj.GetType()
+                            .GetMethods()
+                            .Count();
+
+            result = result - numberOfMethodsComeFromProperties;
+
+            Assert.AreEqual(totalMethodsCount, result);
+        }
+
+        /// <summary>
+        /// At that moment TestClas contain 2 Test methods = fail mean someone changed it
+        /// </summary>
+        [Test]
+        public void PaymentIdTests_VerifyNumberOfTests()
+        {
+            var methodsFromFramework = 4;
+            var expectedMethods = 2;
+            var totalExpectedMethods = methodsFromFramework + expectedMethods;
+
+            var obj = new PaymentIdTests();
+
+            var result = obj.GetType()
+                            .GetMethods()
+                            .Count();
+
+            Assert.AreEqual(totalExpectedMethods, result);
+        }
+
+        /// <summary>
+        /// At that moment TestClass contains 2 TestCase attributes - fail mean someone changed it
+        /// </summary>
+        [Test]
+        public void PaymentIdTests_VeryfyTestCaseAttributes()
+        {
+            var obj = new PaymentIdTests();
+
+            var result = obj.GetType()
+                            .GetMethods()
+                            .Select(x => x.GetCustomAttributes(false)
+                                        .Where(z => z.GetType() == typeof(TestCaseAttribute))
+                                        .Count())
+                            .Sum();
+
+            Assert.AreEqual(2, result);
+        }
+
+        /// <summary>
+        /// At that moment TestClas contain 4 Test methods = fail mean someone changed it
+        /// </summary>
+        [Test]
+        public void PaymentAmountPaidTests_VerifyNumberOfTests()
+        {
+            var methodsFromFramework = 4;
+            var expectedMethods = 4;
+            var totalExpectedMethods = methodsFromFramework + expectedMethods;
+
+            var obj = new PaymentAmountPaidTests();
+
+            var result = obj.GetType()
+                            .GetMethods()
+                            .Count();
+
+            Assert.AreEqual(totalExpectedMethods, result);
+        }
+
+        /// <summary>
+        /// At that moment TestClass contains 2 TestCase attributes - fail mean someone changed it
+        /// </summary>
+        [Test]
+        public void PaymentAmountPaidTests_VeryfyTestCaseAttributes()
+        {
+            var obj = new PaymentAmountPaidTests();
+
+            var result = obj.GetType()
+                            .GetMethods()
+                            .Select(x => x.GetCustomAttributes(false)
+                                        .Where(z => z.GetType() == typeof(TestCaseAttribute))
+                                        .Count())
+                            .Sum();
+
+            Assert.AreEqual(2, result);
+        }
+
+        /// <summary>
+        /// At that moment TestClas contain 1 Test method = fail mean someone changed it
+        /// </summary>
+        [Test]
+        public void PaymentAsDbModelTests_VerifyNumberOfTests()
+        {
+            var methodsFromFramework = 4;
+            var expectedMethods = 1;
+            var totalExpectedMethods = methodsFromFramework + expectedMethods;
+
+            var obj = new PaymentAsDbModelTests();
+
+            var result = obj.GetType()
+                            .GetMethods()
+                            .Count();
+
+            Assert.AreEqual(totalExpectedMethods, result);
+        }
+
+        /// <summary>
+        /// At that moment TestClass contains 0 TestCase attributes - fail mean someone changed it
+        /// </summary>
+        [Test]
+        public void PaymentAsDbModelTests_VeryfyTestCaseAttributes()
+        {
+            var obj = new PaymentAsDbModelTests();
+
+            var result = obj.GetType()
+                            .GetMethods()
+                            .Select(x => x.GetCustomAttributes(false)
+                                        .Where(z => z.GetType() == typeof(TestCaseAttribute))
+                                        .Count())
+                            .Sum();
+
+            Assert.AreEqual(0, result);
+        }
+
+        /// <summary>
+        /// At that moment TestClas contain 6 Test methods = fail mean someone changed it
+        /// </summary>
+        [Test]
+        public void PaymentConstructorTests_VerifyNumberOfTests()
+        {
+            var methodsFromFramework = 4;
+            var expectedMethods = 6;
+            var totalExpectedMethods = methodsFromFramework + expectedMethods;
+
+            var obj = new PaymentConstructorTests();
+
+            var result = obj.GetType()
+                            .GetMethods()
+                            .Count();
+
+            Assert.AreEqual(totalExpectedMethods, result);
+        }
+
+        /// <summary>
+        /// At that moment TestClass contains 0 TestCase attributes - fail mean someone changed it
+        /// </summary>
+        [Test]
+        public void PaymentConstructorTests_VeryfyTestCaseAttributes()
+        {
+            var obj = new PaymentConstructorTests();
+
+            var result = obj.GetType()
+                            .GetMethods()
+                            .Select(x => x.GetCustomAttributes(false)
+                                        .Where(z => z.GetType() == typeof(TestCaseAttribute))
+                                        .Count())
+                            .Sum();
+
+            Assert.AreEqual(0, result);
+        }
+
+        /// <summary>
+        /// At that moment TestClas contain 1 Test method = fail mean someone changed it
+        /// </summary>
+        [Test]
+        public void PaymentIsDeletedTests_VerifyNumberOfTests()
+        {
+            var methodsFromFramework = 4;
+            var expectedMethods = 1;
+            var totalExpectedMethods = methodsFromFramework + expectedMethods;
+
+            var obj = new PaymentIsDeletedTests();
+
+            var result = obj.GetType()
+                            .GetMethods()
+                            .Count();
+
+            Assert.AreEqual(totalExpectedMethods, result);
+        }
+
+        /// <summary>
+        /// At that moment TestClass contains 2 TestCase attributes - fail mean someone changed it
+        /// </summary>
+        [Test]
+        public void PaymentIsDeletedTests_VeryfyTestCaseAttributes()
+        {
+            var obj = new PaymentIsDeletedTests();
+
+            var result = obj.GetType()
+                            .GetMethods()
+                            .Select(x => x.GetCustomAttributes(false)
+                                        .Where(z => z.GetType() == typeof(TestCaseAttribute))
+                                        .Count())
+                            .Sum();
+
+            Assert.AreEqual(2, result);
+        }
+
+        /// <summary>
+        /// At that moment TestClas contain 1 Test method = fail mean someone changed it
+        /// </summary>
+        [Test]
+        public void PaymentWorkerIdTests_VerifyNumberOfTests()
+        {
+            var methodsFromFramework = 4;
+            var expectedMethods = 1;
+            var totalExpectedMethods = methodsFromFramework + expectedMethods;
+
+            var obj = new PaymentWorkerIdTests();
+
+            var result = obj.GetType()
+                            .GetMethods()
+                            .Count();
+
+            Assert.AreEqual(totalExpectedMethods, result);
+        }
+
+        /// <summary>
+        /// At that moment TestClass contains 2 TestCase attributes - fail mean someone changed it
+        /// </summary>
+        [Test]
+        public void PaymentWorkerIdTests_VeryfyTestCaseAttributes()
+        {
+            var obj = new PaymentWorkerIdTests();
+
+            var result = obj.GetType()
+                            .GetMethods()
+                            .Select(x => x.GetCustomAttributes(false)
+                                        .Where(z => z.GetType() == typeof(TestCaseAttribute))
+                                        .Count())
+                            .Sum();
+
+            Assert.AreEqual(2, result);
+        }
+
+        /// <summary>
+        /// At that moment TestClas contain 1 Test method = fail mean someone changed it
+        /// </summary>
+        [Test]
+        public void PaymentWorkerTests_VerifyNumberOfTests()
+        {
+            var methodsFromFramework = 4;
+            var expectedMethods = 1;
+            var totalExpectedMethods = methodsFromFramework + expectedMethods;
+
+            var obj = new PaymentWorkerTests();
+
+            var result = obj.GetType()
+                            .GetMethods()
+                            .Count();
+
+            Assert.AreEqual(totalExpectedMethods, result);
+        }
+
+        /// <summary>
+        /// At that moment TestClass contains 0 TestCase attributes - fail mean someone changed it
+        /// </summary>
+        [Test]
+        public void PaymentWorkerTests_VeryfyTestCaseAttributes()
+        {
+            var obj = new PaymentWorkerTests();
+
+            var result = obj.GetType()
+                            .GetMethods()
+                            .Select(x => x.GetCustomAttributes(false)
+                                        .Where(z => z.GetType() == typeof(TestCaseAttribute))
+                                        .Count())
+                            .Sum();
+
+            Assert.AreEqual(0, result);
+        }
+    }
+}
