ae9290a81431b7587fb26367add2b0b9172fcf05
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-21 17:58:52 +0200

    added ReceivedPayment AmountPaid Tests

---
+++ WhenItsDone/Tests/LibTests/WhenItsDone.Models.Tests/ReceivedPaymentTests/ReceivedPaymentAmountPaidTests.cs
@@ -0,0 +1,67 @@
+﻿using NUnit.Framework;
+using System.Linq;
+using WhenItsDone.Models.Constants;
+
+namespace WhenItsDone.Models.Tests.ReceivedPaymentTests
+{
+    [TestFixture]
+    public class ReceivedPaymentAmountPaidTests
+    {
+        [Test]
+        public void AmountPaid_ShouldHave_RangeAttribute()
+        {
+            var obj = new ReceivedPayment();
+
+            var result = obj.GetType()
+                            .GetProperty("AmountPaid")
+                            .GetCustomAttributes(false)
+                            .Where(x => x.GetType() == typeof(System.ComponentModel.DataAnnotations.RangeAttribute))
+                            .Any();
+
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void AmountPaid_ShouldHave_RightMinValueFor_RangeAttribute()
+        {
+            var obj = new ReceivedPayment();
+
+            var result = obj.GetType()
+                            .GetProperty("AmountPaid")
+                            .GetCustomAttributes(false)
+                            .Where(x => x.GetType() == typeof(System.ComponentModel.DataAnnotations.RangeAttribute))
+                            .Select(x => (System.ComponentModel.DataAnnotations.RangeAttribute)x)
+                            .SingleOrDefault();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(ValidationConstants.AmountPaidMinValue, result.Minimum);
+        }
+
+        [Test]
+        public void AmountPaid_ShouldHave_RightMaxValueFor_RangeAttribute()
+        {
+            var obj = new ReceivedPayment();
+
+            var result = obj.GetType()
+                            .GetProperty("AmountPaid")
+                            .GetCustomAttributes(false)
+                            .Where(x => x.GetType() == typeof(System.ComponentModel.DataAnnotations.RangeAttribute))
+                            .Select(x => (System.ComponentModel.DataAnnotations.RangeAttribute)x)
+                            .SingleOrDefault();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(ValidationConstants.AmountPaidMaxValue, result.Maximum);
+        }
+
+        [TestCase(3532)]
+        [TestCase(1)]
+        public void AmountPaid_GetAndSetShould_WorkProperly(int randomNumber)
+        {
+            var obj = new ReceivedPayment();
+
+            obj.AmountPaid = randomNumber;
+
+            Assert.AreEqual(randomNumber, obj.AmountPaid);
+        }
+    }
+}
