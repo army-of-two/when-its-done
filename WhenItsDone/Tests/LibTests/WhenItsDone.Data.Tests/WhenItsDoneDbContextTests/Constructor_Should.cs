c23d3d3a7fb48934e8f5567f0d95c846a4717369
shakuu <todorov.h@gmail.com>
2017-01-21 19:37:24 +0200

    added addresses tests

--- WhenItsDone/Tests/LibTests/WhenItsDone.Data.Tests/WhenItsDoneDbContextTests/Constructor_Should.cs
+++ WhenItsDone/Tests/LibTests/WhenItsDone.Data.Tests/WhenItsDoneDbContextTests/Constructor_Should.cs
@@ -3,6 +3,8 @@
 using NUnit.Framework;
 
 using WhenItsDone.Data.Contracts;
+using System.Reflection;
+using WhenItsDone.Models;
 
 namespace WhenItsDone.Data.Tests.WhenItsDoneDbContextTests
 {
@@ -10,7 +12,7 @@ namespace WhenItsDone.Data.Tests.WhenItsDoneDbContextTests
     public class Constructor_Should
     {
         [Test]
-        public void ShouldCreateAValidCorrectInstanceOfDbContext()
+        public void ShouldCreateAValidInstanceOfDbContext()
         {
             var whenItsDoneDbContext = new WhenItsDoneDbContext();
 
@@ -18,11 +20,50 @@ namespace WhenItsDone.Data.Tests.WhenItsDoneDbContextTests
         }
 
         [Test]
-        public void ShouldCreateAValidCorrectInstanceOfIWhenItsDoneDbContext()
+        public void ShouldCreateAValidInstanceOfIWhenItsDoneDbContext()
         {
             var whenItsDoneDbContext = new WhenItsDoneDbContext();
 
             Assert.That(whenItsDoneDbContext, Is.InstanceOf<IWhenItsDoneDbContext>());
+        }
+
+        [Test]
+        public void ShouldCreateAValidInstance_WithAddressesProperty()
+        {
+            var whenItsDoneDbContext = new WhenItsDoneDbContext();
+
+            var propertyName = "Addresses";
+            var bindingFlags = BindingFlags.Public | BindingFlags.Instance;
+
+            var addressesProperty = whenItsDoneDbContext.GetType().GetProperty(propertyName, bindingFlags);
+
+            Assert.That(addressesProperty, Is.Not.Null);
+        }
+
+        [Test]
+        public void ShouldCreateAValidInstance_WithAddressVirtualProperty()
+        {
+            var whenItsDoneDbContext = new WhenItsDoneDbContext();
+
+            var propertyName = "Addresses";
+            var bindingFlags = BindingFlags.Public | BindingFlags.Instance;
+
+            var addressesProperty = whenItsDoneDbContext.GetType().GetProperty(propertyName, bindingFlags);
+
+            Assert.That(addressesProperty.GetGetMethod().IsVirtual, Is.True);
+        }
+
+        [Test]
+        public void ShouldCreateAValidInstance_WithAddressVirtualProperty_OfTypeIDbSetAddresses()
+        {
+            var whenItsDoneDbContext = new WhenItsDoneDbContext();
+
+            var propertyName = "Addresses";
+            var bindingFlags = BindingFlags.Public | BindingFlags.Instance;
+
+            var addressesProperty = whenItsDoneDbContext.GetType().GetProperty(propertyName, bindingFlags);
+
+            Assert.That(addressesProperty.PropertyType, Is.EqualTo(typeof(IDbSet<Address>)));
         }
     }
 }
