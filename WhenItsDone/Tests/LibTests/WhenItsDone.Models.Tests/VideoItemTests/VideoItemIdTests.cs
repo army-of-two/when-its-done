55f27539fc43e53de2261d61a195d825e10617fb
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-21 18:25:36 +0200

    added VideoItem Id Tests

---
+++ WhenItsDone/Tests/LibTests/WhenItsDone.Models.Tests/VideoItemTests/VideoItemIdTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace WhenItsDone.Models.Tests.VideoItemTests
+{
+    [TestFixture]
+    public class VideoItemIdTests
+    {
+        [Test]
+        public void Id_ShouldHave_KeyAttribute()
+        {
+            var obj = new VideoItem();
+
+            var result = obj.GetType()
+                            .GetProperty("Id")
+                            .GetCustomAttributes(false)
+                            .Where(x => x.GetType() == typeof(KeyAttribute))
+                            .Any();
+
+            Assert.IsTrue(result);
+        }
+
+        [TestCase(4)]
+        [TestCase(66665666)]
+        public void Id_GetAndSetShould_WorkProperly(int randomNumber)
+        {
+            var obj = new VideoItem();
+
+            obj.Id = randomNumber;
+
+            Assert.AreEqual(randomNumber, obj.Id);
+        }
+    }
+}
