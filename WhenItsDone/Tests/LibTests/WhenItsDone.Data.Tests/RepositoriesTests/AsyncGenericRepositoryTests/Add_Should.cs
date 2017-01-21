4801283c49041eaaebcf4e313e0c469189110815
shakuu <todorov.h@gmail.com>
2017-01-22 00:44:42 +0200

    added more AsyncGenericRepository.Add tests

--- WhenItsDone/Tests/LibTests/WhenItsDone.Data.Tests/RepositoriesTests/AsyncGenericRepositoryTests/Add_Should.cs
+++ WhenItsDone/Tests/LibTests/WhenItsDone.Data.Tests/RepositoriesTests/AsyncGenericRepositoryTests/Add_Should.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 using System.Reflection;
-using System.Threading.Tasks;
 
 using Moq;
 using NUnit.Framework;
@@ -9,7 +9,6 @@ using NUnit.Framework;
 using WhenItsDone.Data.Contracts;
 using WhenItsDone.Data.Repositories;
 using WhenItsDone.Models.Contracts;
-using System.Data.Entity.Infrastructure;
 
 namespace WhenItsDone.Data.Tests.RepositoriesTests.AsyncGenericRepositoryTests
 {
@@ -71,6 +70,42 @@ namespace WhenItsDone.Data.Tests.RepositoriesTests.AsyncGenericRepositoryTests
             }
 
             mockDbContext.Verify(mock => mock.Entry(It.IsAny<IDbModel>()), Times.Once());
+        }
+
+        [Test]
+        public void InvokeDbContextEntryMethodWithCorrectParameter_WhenParametersAreValid()
+        {
+            // This is needed to create the instance.
+            // DbContext.Set<>() returns DbSet rather than IDbSet<>.
+            var ctorParameters = new Type[] { };
+            var ctorBindingFlags = BindingFlags.NonPublic | BindingFlags.Instance;
+            var dbSetConstructor = typeof(DbSet<IDbModel>).GetConstructor(ctorBindingFlags, null, ctorParameters, null);
+            var fakeDbSet = (DbSet<IDbModel>)dbSetConstructor.Invoke(null);
+
+            var entity = (DbEntityEntry<IDbModel>)System.Runtime.Serialization.FormatterServices.GetSafeUninitializedObject(typeof(DbEntityEntry<IDbModel>));
+            var mockDbContext = new Mock<IWhenItsDoneDbContext>();
+            mockDbContext.Setup(mock => mock.Set<IDbModel>()).Returns(fakeDbSet);
+            mockDbContext.Setup(mock => mock.Entry(It.IsAny<IDbModel>())).Returns(entity);
+
+            var asyncGenericRepositoryInstace = new AsyncGenericRepository<IDbModel>(mockDbContext.Object);
+
+            var fakeDbModel = new Mock<IDbModel>();
+
+            try
+            {
+                asyncGenericRepositoryInstace.Add(fakeDbModel.Object);
+            }
+            catch (NullReferenceException)
+            {
+                // This will throw due to not being able to instantiate an InternalEntityEntry object.
+                // Because of that all DbEntityEntry<T> public properties throw a NullReferenceException.
+
+                // namespace System.Data.Entity.Internal
+                // internal class InternalEntityEntry
+                // https://github.com/mono/entityframework/blob/master/src/EntityFramework/Internal/EntityEntries/InternalEntityEntry.cs
+            }
+
+            mockDbContext.Verify(mock => mock.Entry(fakeDbModel.Object), Times.Once());
         }
     }
 }
