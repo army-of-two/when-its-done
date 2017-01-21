2028afaf1fc2007c580e9eb8b31201773ee3fb30
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-21 11:26:52 +0200

    added ContactInformation PhoneNumber, Email, IsDeleted tests

--- WhenItsDone/Tests/LibTests/WhenItsDone.Models.Tests/ContactInformationTests/ContactInformationIdTests.cs
+++ WhenItsDone/Tests/LibTests/WhenItsDone.Models.Tests/ContactInformationTests/ContactInformationIdTests.cs
@@ -10,7 +10,7 @@ namespace WhenItsDone.Models.Tests.ContactInformationTests
         [Test]
         public void Id_ShouldHave_KeyAttribute()
         {
-            var obj = new Client();
+            var obj = new ContactInformation();
 
             var result = obj.GetType()
                             .GetProperty("Id")
@@ -25,7 +25,7 @@ namespace WhenItsDone.Models.Tests.ContactInformationTests
         [TestCase(3123)]
         public void Id_GetAndSetShould_WorkProperly(int randomNumber)
         {
-            var obj = new Client();
+            var obj = new ContactInformation();
 
             obj.Id = randomNumber;
 
