02e8d5914451b870097584b06b01e0052fdeaa61
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-21 17:55:31 +0200

    added ReceivedPayment Client Tests

---
+++ WhenItsDone/Tests/LibTests/WhenItsDone.Models.Tests/ReceivedPaymentTests/ReceivedPaymentClientTests.cs
@@ -0,0 +1,21 @@
+﻿using Moq;
+using NUnit.Framework;
+
+namespace WhenItsDone.Models.Tests.ReceivedPaymentTests
+{
+    [TestFixture]
+    class ReceivedPaymentClientTests
+    {
+        [Test]
+        public void Client_GetAndSetShould_WorkProperly()
+        {
+            var mockedClient = new Mock<Client>();
+
+            var obj = new ReceivedPayment();
+
+            obj.Client = mockedClient.Object;
+
+            Assert.AreSame(mockedClient.Object, obj.Client);
+        }
+    }
+}
