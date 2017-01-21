fc37072e166377254880a453b0ea2c96a343a998
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-21 10:45:12 +0200

    added ContactInformation.Id tests

---
+++ WhenItsDone/Tests/LibTests/WhenItsDone.Models.Tests/ContactInformationTests/ContactInformationIdTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace WhenItsDone.Models.Tests.ContactInformationTests
+{
+    [TestFixture]
+    public class ContactInformationIdTests
+    {
+        [Test]
+        public void Id_ShouldHave_KeyAttribute()
+        {
+            var obj = new Client();
+
+            var result = obj.GetType()
+                            .GetProperty("Id")
+                            .GetCustomAttributes(false)
+                            .Where(x => x.GetType() == typeof(KeyAttribute))
+                            .Any();
+
+            Assert.IsTrue(result);
+        }
+
+        [TestCase(54657)]
+        [TestCase(3123)]
+        public void Id_GetAndSetShould_WorkProperly(int randomNumber)
+        {
+            var obj = new Client();
+
+            obj.Id = randomNumber;
+
+            Assert.AreEqual(randomNumber, obj.Id);
+        }
+    }
+}
