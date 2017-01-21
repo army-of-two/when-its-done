5aabaa007576e01b044e5286e7a2abd1548b9673
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-21 09:22:53 +0200

    added Client.ContactInformation IsVirtual test

--- WhenItsDone/Tests/LibTests/WhenItsDone.Models.Tests/ClientTests/ClientContactInformationTests.cs
+++ WhenItsDone/Tests/LibTests/WhenItsDone.Models.Tests/ClientTests/ClientContactInformationTests.cs
@@ -1,5 +1,6 @@
 ﻿using Moq;
 using NUnit.Framework;
+using System.Linq;
 
 namespace WhenItsDone.Models.Tests.ClientTests
 {
@@ -16,6 +17,19 @@ namespace WhenItsDone.Models.Tests.ClientTests
             obj.ContactInformation = mockedInfo.Object;
 
             Assert.AreSame(mockedInfo.Object, obj.ContactInformation);
+        }
+
+        [Test]
+        public void ContactInformation_ShouldBe_Virtual()
+        {
+            var obj = new Client();
+
+            var result = obj.GetType()
+                            .GetProperty("ContactInformation")
+                            .GetAccessors()
+                            .Any(x => x.IsVirtual);
+
+            Assert.IsTrue(result);
         }
     }
 }
