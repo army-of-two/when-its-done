5243e575ce04d8d08eefeafc7063320162e0c075
shakuu <todorov.h@gmail.com>
2017-01-21 21:25:58 +0200

    added more AsyncGenericRepository.GetByIdAsync tests

--- WhenItsDone/Tests/LibTests/WhenItsDone.Data.Tests/RepositoriesTests/AsyncGenericRepositoryTests/GetByIdAsync_Should.cs
+++ WhenItsDone/Tests/LibTests/WhenItsDone.Data.Tests/RepositoriesTests/AsyncGenericRepositoryTests/GetByIdAsync_Should.cs
@@ -42,5 +42,34 @@ namespace WhenItsDone.Data.Tests.RepositoriesTests.AsyncGenericRepositoryTests
                 () => asyncGenericRepositoryInstace.GetByIdAsync(invalidId),
                 Throws.InstanceOf<ArgumentException>().With.Message.Contains("Id must be a positive integer."));
         }
+
+        [Test]
+        public void ShouldInvokeDbSetFindMethodOnce_WhenParametersAreValid()
+        {
+            // This is needed to create the instance.
+            // DbContext.Set<>() returns DbSet rather than IDbSet<>.
+            var ctorBindingFlags = BindingFlags.NonPublic | BindingFlags.Instance;
+            var dbSetConstructor = typeof(DbSet<IDbModel>).GetConstructor(ctorBindingFlags, null, new Type[] { }, null);
+            var fakeDbSet = (DbSet<IDbModel>)dbSetConstructor.Invoke(null);
+
+            var mockDbContext = new Mock<IWhenItsDoneDbContext>();
+            mockDbContext.Setup(mock => mock.Set<IDbModel>()).Returns(fakeDbSet);
+
+            var asyncGenericRepositoryInstace = new AsyncGenericRepository<IDbModel>(mockDbContext.Object);
+
+            // This is needed to mock the IDbSet<> object.
+            var mockDbSet = new Mock<IDbSet<IDbModel>>();
+            var fieldName = "dbSet";
+            var bindingFlags = BindingFlags.NonPublic | BindingFlags.Instance;
+            var dbSetField = asyncGenericRepositoryInstace.GetType().GetField(fieldName, bindingFlags);
+            dbSetField.SetValue(asyncGenericRepositoryInstace, mockDbSet.Object);
+
+            mockDbSet.Setup(mock => mock.Find(It.IsAny<int>())).Returns<IDbModel>(null);
+
+            var validId = 42;
+            asyncGenericRepositoryInstace.GetByIdAsync(validId);
+
+            mockDbSet.Verify(mock => mock.Find(It.IsAny<int>()), Times.Once());
+        }
     }
 }
