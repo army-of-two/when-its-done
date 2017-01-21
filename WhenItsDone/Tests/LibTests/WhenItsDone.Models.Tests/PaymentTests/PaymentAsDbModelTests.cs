1f1c51ab3acc211da4bbd53481b9bd030bfd552f
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-21 14:21:49 +0200

    added Payment as DbModel tests

---
+++ WhenItsDone/Tests/LibTests/WhenItsDone.Models.Tests/PaymentTests/PaymentAsDbModelTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System.Linq;
+using WhenItsDone.Models.Contracts;
+
+namespace WhenItsDone.Models.Tests.PaymentTests
+{
+    [TestFixture]
+    public class PaymentAsDbModelTests
+    {
+        [Test]
+        public void PaymentClass_ShouldImplement_IDbModelInterface()
+        {
+            var obj = new Payment();
+
+            var result = obj.GetType()
+                            .GetInterfaces()
+                            .Where(x => x == typeof(IDbModel))
+                            .Any();
+
+            Assert.IsTrue(result);
+        }
+    }
+}
