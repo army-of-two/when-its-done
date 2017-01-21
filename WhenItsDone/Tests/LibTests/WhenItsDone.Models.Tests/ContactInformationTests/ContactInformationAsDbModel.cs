292aa0177300c819e3c5efb4dfecd2dd52fab2ae
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-21 10:10:55 +0200

    added ContactInformation as DbModel tests

---
+++ WhenItsDone/Tests/LibTests/WhenItsDone.Models.Tests/ContactInformationTests/ContactInformationAsDbModel.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System.Linq;
+using WhenItsDone.Models.Contracts;
+
+namespace WhenItsDone.Models.Tests.ContactInformationTests
+{
+    [TestFixture]
+    public class ContactInformationAsDbModel
+    {
+        [Test]
+        public void ContactInformationClass_ShouldImplement_IDbModelInterface()
+        {
+            var obj = new ContactInformation();
+
+            var result = obj.GetType()
+                            .GetInterfaces()
+                            .Any(x => x == typeof(IDbModel));
+
+            Assert.IsTrue(result);
+        }
+    }
+}
