33225aa9ff0c6b6ac7c512b0dc2c1eccdae369ce
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-21 17:04:08 +0200

    Added PhotoItem.Url test - unfinished coz class need validations

---
+++ WhenItsDone/Tests/LibTests/WhenItsDone.Models.Tests/PhotoItemTests/PhotoItemUrlTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace WhenItsDone.Models.Tests.PhotoItemTests
+{
+    [TestFixture]
+    public class PhotoItemUrlTests
+    {
+        [TestCase("Merry Christmas")]
+        [TestCase("abv.bg/bg/abv/default.aspx")]
+        public void Url_GetAndSetShould_WorkProperly(string randomString)
+        {
+            var obj = new PhotoItem();
+
+            obj.Url = randomString;
+
+            Assert.AreEqual(randomString, obj.Url);
+        }
+    }
+}
