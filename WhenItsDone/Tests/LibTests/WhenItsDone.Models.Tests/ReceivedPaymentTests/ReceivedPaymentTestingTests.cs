821a4d796cb3309fc3eab390179ffdf3d69090fc
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-21 18:11:24 +0200

    added ReceivedPayment test of tests

---
+++ WhenItsDone/Tests/LibTests/WhenItsDone.Models.Tests/ReceivedPaymentTests/ReceivedPaymentTestingTests.cs
@@ -0,0 +1,335 @@
+﻿using NUnit.Framework;
+using System.Linq;
+
+namespace WhenItsDone.Models.Tests.ReceivedPaymentTests
+{
+    [TestFixture]
+    public class ReceivedPaymentTestingTests
+    {
+        /// <summary>
+        /// At that moment ReceivedPayment class have 5 tested properties .. if another property is added this test will fail
+        /// That mean new tests are REQUIRED!
+        /// </summary>
+        [Test]
+        public void ReceivedPayment_VerifyNumberOfProperties()
+        {
+            var obj = new ReceivedPayment();
+
+            var result = obj.GetType()
+                            .GetProperties()
+                            .Count();
+
+            Assert.AreEqual(5, result);
+        }
+
+        /// <summary>
+        /// At that moment ReceivedPayment class have 1 tested constructor
+        /// </summary>
+        [Test]
+        public void ReceivedPayment_VerifyNumberOfConstructors()
+        {
+            var obj = new ReceivedPayment();
+
+            var methodsCount = obj.GetType()
+                                    .GetMethods()
+                                    .Count();
+
+            var propertiesCount = obj.GetType()
+                                    .GetProperties()
+                                    .Count();
+
+            var result = obj.GetType()
+                            .GetMembers()
+                            .Count();
+
+            result = result - propertiesCount - methodsCount;
+
+            Assert.AreEqual(1, result);
+        }
+
+        /// <summary>
+        /// At that moment ReceivedPayment class have 0 tested methods
+        /// </summary>
+        [Test]
+        public void ReceivedPayment_VerifyNumberOfMethods()
+        {
+            var methodsFromFramework = 4;
+            var expectedMethodsCount = 0;
+            var totalMethodsCount = methodsFromFramework + expectedMethodsCount;
+
+            var obj = new ReceivedPayment();
+
+            var numberOfMethodsComeFromProperties = obj.GetType()
+                                                        .GetProperties()
+                                                        .Select(x => 2)
+                                                        .Sum();
+
+            var result = obj.GetType()
+                            .GetMethods()
+                            .Count();
+
+            result = result - numberOfMethodsComeFromProperties;
+
+            Assert.AreEqual(totalMethodsCount, result);
+        }
+
+        /// <summary>
+        /// At that moment TestClas contain 2 Test methods = fail mean someone changed it
+        /// </summary>
+        [Test]
+        public void ReceivedPaymentIdTests_VerifyNumberOfTests()
+        {
+            var methodsFromFramework = 4;
+            var expectedMethods = 2;
+            var totalExpectedMethods = methodsFromFramework + expectedMethods;
+
+            var obj = new ReceivedPaymentIdTests();
+
+            var result = obj.GetType()
+                            .GetMethods()
+                            .Count();
+
+            Assert.AreEqual(totalExpectedMethods, result);
+        }
+
+        /// <summary>
+        /// At that moment TestClass contains 2 TestCase attributes - fail mean someone changed it
+        /// </summary>
+        [Test]
+        public void ReceivedPaymentIdTests_VeryfyTestCaseAttributes()
+        {
+            var obj = new ReceivedPaymentIdTests();
+
+            var result = obj.GetType()
+                            .GetMethods()
+                            .Select(x => x.GetCustomAttributes(false)
+                                        .Where(z => z.GetType() == typeof(TestCaseAttribute))
+                                        .Count())
+                            .Sum();
+
+            Assert.AreEqual(2, result);
+        }
+
+        /// <summary>
+        /// At that moment TestClas contain 4 Test methods = fail mean someone changed it
+        /// </summary>
+        [Test]
+        public void ReceivedPaymentAmountPaidTests_VerifyNumberOfTests()
+        {
+            var methodsFromFramework = 4;
+            var expectedMethods = 4;
+            var totalExpectedMethods = methodsFromFramework + expectedMethods;
+
+            var obj = new ReceivedPaymentAmountPaidTests();
+
+            var result = obj.GetType()
+                            .GetMethods()
+                            .Count();
+
+            Assert.AreEqual(totalExpectedMethods, result);
+        }
+
+        /// <summary>
+        /// At that moment TestClass contains 2 TestCase attributes - fail mean someone changed it
+        /// </summary>
+        [Test]
+        public void ReceivedPaymentAmountPaidTests_VeryfyTestCaseAttributes()
+        {
+            var obj = new ReceivedPaymentAmountPaidTests();
+
+            var result = obj.GetType()
+                            .GetMethods()
+                            .Select(x => x.GetCustomAttributes(false)
+                                        .Where(z => z.GetType() == typeof(TestCaseAttribute))
+                                        .Count())
+                            .Sum();
+
+            Assert.AreEqual(2, result);
+        }
+
+        /// <summary>
+        /// At that moment TestClas contain 1 Test method = fail mean someone changed it
+        /// </summary>
+        [Test]
+        public void ReceivedPaymentAsDbModelTests_VerifyNumberOfTests()
+        {
+            var methodsFromFramework = 4;
+            var expectedMethods = 1;
+            var totalExpectedMethods = methodsFromFramework + expectedMethods;
+
+            var obj = new ReceivedPaymentAsDbModelTests();
+
+            var result = obj.GetType()
+                            .GetMethods()
+                            .Count();
+
+            Assert.AreEqual(totalExpectedMethods, result);
+        }
+
+        /// <summary>
+        /// At that moment TestClass contains 0 TestCase attributes - fail mean someone changed it
+        /// </summary>
+        [Test]
+        public void ReceivedPaymentAsDbModelTests_VeryfyTestCaseAttributes()
+        {
+            var obj = new ReceivedPaymentAsDbModelTests();
+
+            var result = obj.GetType()
+                            .GetMethods()
+                            .Select(x => x.GetCustomAttributes(false)
+                                        .Where(z => z.GetType() == typeof(TestCaseAttribute))
+                                        .Count())
+                            .Sum();
+
+            Assert.AreEqual(0, result);
+        }
+
+        /// <summary>
+        /// At that moment TestClas contain 6 Test methods = fail mean someone changed it
+        /// </summary>
+        [Test]
+        public void ReceivedPaymentConstructorTests_VerifyNumberOfTests()
+        {
+            var methodsFromFramework = 4;
+            var expectedMethods = 6;
+            var totalExpectedMethods = methodsFromFramework + expectedMethods;
+
+            var obj = new ReceivedPaymentConstructorTests();
+
+            var result = obj.GetType()
+                            .GetMethods()
+                            .Count();
+
+            Assert.AreEqual(totalExpectedMethods, result);
+        }
+
+        /// <summary>
+        /// At that moment TestClass contains 0 TestCase attributes - fail mean someone changed it
+        /// </summary>
+        [Test]
+        public void ReceivedPaymentConstructorTests_VeryfyTestCaseAttributes()
+        {
+            var obj = new ReceivedPaymentConstructorTests();
+
+            var result = obj.GetType()
+                            .GetMethods()
+                            .Select(x => x.GetCustomAttributes(false)
+                                        .Where(z => z.GetType() == typeof(TestCaseAttribute))
+                                        .Count())
+                            .Sum();
+
+            Assert.AreEqual(0, result);
+        }
+
+        /// <summary>
+        /// At that moment TestClas contain 1 Test method = fail mean someone changed it
+        /// </summary>
+        [Test]
+        public void ReceivedPaymentIsDeletedTests_VerifyNumberOfTests()
+        {
+            var methodsFromFramework = 4;
+            var expectedMethods = 1;
+            var totalExpectedMethods = methodsFromFramework + expectedMethods;
+
+            var obj = new ReceivedPaymentIsDeletedTests();
+
+            var result = obj.GetType()
+                            .GetMethods()
+                            .Count();
+
+            Assert.AreEqual(totalExpectedMethods, result);
+        }
+
+        /// <summary>
+        /// At that moment TestClass contains 2 TestCase attributes - fail mean someone changed it
+        /// </summary>
+        [Test]
+        public void ReceivedPaymentIsDeletedTests_VeryfyTestCaseAttributes()
+        {
+            var obj = new ReceivedPaymentIsDeletedTests();
+
+            var result = obj.GetType()
+                            .GetMethods()
+                            .Select(x => x.GetCustomAttributes(false)
+                                        .Where(z => z.GetType() == typeof(TestCaseAttribute))
+                                        .Count())
+                            .Sum();
+
+            Assert.AreEqual(2, result);
+        }
+
+        /// <summary>
+        /// At that moment TestClas contain 1 Test method = fail mean someone changed it
+        /// </summary>
+        [Test]
+        public void ReceivedPaymentClientIdTests_VerifyNumberOfTests()
+        {
+            var methodsFromFramework = 4;
+            var expectedMethods = 1;
+            var totalExpectedMethods = methodsFromFramework + expectedMethods;
+
+            var obj = new ReceivedPaymentClientIdTests();
+
+            var result = obj.GetType()
+                            .GetMethods()
+                            .Count();
+
+            Assert.AreEqual(totalExpectedMethods, result);
+        }
+
+        /// <summary>
+        /// At that moment TestClass contains 2 TestCase attributes - fail mean someone changed it
+        /// </summary>
+        [Test]
+        public void ReceivedPaymentClientIdTests_VeryfyTestCaseAttributes()
+        {
+            var obj = new ReceivedPaymentClientIdTests();
+
+            var result = obj.GetType()
+                            .GetMethods()
+                            .Select(x => x.GetCustomAttributes(false)
+                                        .Where(z => z.GetType() == typeof(TestCaseAttribute))
+                                        .Count())
+                            .Sum();
+
+            Assert.AreEqual(2, result);
+        }
+
+        /// <summary>
+        /// At that moment TestClas contain 1 Test method = fail mean someone changed it
+        /// </summary>
+        [Test]
+        public void ReceivedPaymentClientTests_VerifyNumberOfTests()
+        {
+            var methodsFromFramework = 4;
+            var expectedMethods = 1;
+            var totalExpectedMethods = methodsFromFramework + expectedMethods;
+
+            var obj = new ReceivedPaymentClientTests();
+
+            var result = obj.GetType()
+                            .GetMethods()
+                            .Count();
+
+            Assert.AreEqual(totalExpectedMethods, result);
+        }
+
+        /// <summary>
+        /// At that moment TestClass contains 0 TestCase attributes - fail mean someone changed it
+        /// </summary>
+        [Test]
+        public void ReceivedPaymentClientTests_VeryfyTestCaseAttributes()
+        {
+            var obj = new ReceivedPaymentClientTests();
+
+            var result = obj.GetType()
+                            .GetMethods()
+                            .Select(x => x.GetCustomAttributes(false)
+                                        .Where(z => z.GetType() == typeof(TestCaseAttribute))
+                                        .Count())
+                            .Sum();
+
+            Assert.AreEqual(0, result);
+        }
+    }
+}
