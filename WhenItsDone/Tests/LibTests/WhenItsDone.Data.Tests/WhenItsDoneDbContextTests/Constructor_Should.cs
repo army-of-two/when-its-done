bf362048849a70e820657e72393c452899385b7d
shakuu <todorov.h@gmail.com>
2017-01-21 19:28:36 +0200

    added EF to data.tests assembly

---
+++ WhenItsDone/Tests/LibTests/WhenItsDone.Data.Tests/WhenItsDoneDbContextTests/Constructor_Should.cs
@@ -0,0 +1,28 @@
+﻿using System.Data.Entity;
+
+using NUnit.Framework;
+
+using WhenItsDone.Data.Contracts;
+
+namespace WhenItsDone.Data.Tests.WhenItsDoneDbContextTests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+        [Test]
+        public void ShouldCreateAValidCorrectInstanceOfDbContext()
+        {
+            var whenItsDoneDbContext = new WhenItsDoneDbContext();
+
+            Assert.That(whenItsDoneDbContext, Is.InstanceOf<DbContext>());
+        }
+
+        [Test]
+        public void ShouldCreateAValidCorrectInstanceOfIWhenItsDoneDbContext()
+        {
+            var whenItsDoneDbContext = new WhenItsDoneDbContext();
+
+            Assert.That(whenItsDoneDbContext, Is.InstanceOf<IWhenItsDoneDbContext>());
+        }
+    }
+}
