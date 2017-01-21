86b8dc266fca9e66432f246ee09124a5ac16c3b4
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-21 13:27:54 +0200

    added Job.ClientId tests

---
+++ WhenItsDone/Tests/LibTests/WhenItsDone.Models.Tests/JobTests/JobClientIdTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace WhenItsDone.Models.Tests.JobTests
+{
+    [TestFixture]
+    public class JobClientIdTests
+    {
+        [TestCase(6)]
+        [TestCase(9999991)]
+        public void ClientId_GetAndSetShould_WorkProperly(int randomNumber)
+        {
+            var obj = new Job();
+
+            obj.ClientId = randomNumber;
+
+            Assert.AreEqual(randomNumber, obj.ClientId);
+        }
+    }
+}
