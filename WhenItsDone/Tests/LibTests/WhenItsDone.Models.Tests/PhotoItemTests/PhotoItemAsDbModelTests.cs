197780fe020dc5907f230520ec5ca3388f7784ad
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-21 15:28:59 +0200

    added PhotoItem as DbModel tests

---
+++ WhenItsDone/Tests/LibTests/WhenItsDone.Models.Tests/PhotoItemTests/PhotoItemAsDbModelTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System.Linq;
+using WhenItsDone.Models.Contracts;
+
+namespace WhenItsDone.Models.Tests.PhotoItemTests
+{
+    [TestFixture]
+    public class PhotoItemAsDbModelTests
+    {
+        [Test]
+        public void PhotoItem_ShouldImplement_IDbModelInterface()
+        {
+            var obj = new PhotoItem();
+
+            var result = obj.GetType()
+                            .GetInterfaces()
+                            .Where(x => x == typeof(IDbModel))
+                            .Any();
+
+            Assert.IsTrue(result);
+        }
+    }
+}
