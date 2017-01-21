d5734db52773fdaebc555906e084da3a33a78828
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-21 17:50:22 +0200

    added ReceivedPayment IsDeleted Tests

---
+++ WhenItsDone/Tests/LibTests/WhenItsDone.Models.Tests/ReceivedPaymentTests/ReceivedPaymentIsDeletedTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace WhenItsDone.Models.Tests.ReceivedPaymentTests
+{
+    [TestFixture]
+    public class ReceivedPaymentIsDeletedTests
+    {
+        [TestCase(true)]
+        [TestCase(false)]
+        public void IsDeleted_GetAndSetShould_WorkProperly(bool value)
+        {
+            var obj = new ReceivedPayment();
+
+            obj.IsDeleted = value;
+
+            Assert.AreEqual(value, obj.IsDeleted);
+        }
+    }
+}
+
