554e033280f02927464253446d2f73cc3c33bdf5
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-22 10:26:29 +0200

    added Worker ContactInformationId Tests

---
+++ WhenItsDone/Tests/LibTests/WhenItsDone.Models.Tests/WorkerTests/WorkerContactInformationIdTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace WhenItsDone.Models.Tests.WorkerTests
+{
+    [TestFixture]
+    public class WorkerContactInformationIdTests
+    {
+        [TestCase(323213)]
+        [TestCase(-37)]
+        public void ContactInformationId_GetAndSetShould_WorkProperly(int randomNumber)
+        {
+            var obj = new Worker();
+
+            obj.ContactInformationId = randomNumber;
+
+            Assert.AreEqual(randomNumber, obj.ContactInformationId);
+        }
+    }
+}
