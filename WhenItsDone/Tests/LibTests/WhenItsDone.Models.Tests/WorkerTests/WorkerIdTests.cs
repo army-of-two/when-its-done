8b362d4d6e011500b6351121cf96db7a78afaa8e
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-22 09:47:01 +0200

    added Worker Id Tests

---
+++ WhenItsDone/Tests/LibTests/WhenItsDone.Models.Tests/WorkerTests/WorkerIdTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace WhenItsDone.Models.Tests.WorkerTests
+{
+    [TestFixture]
+    public class WorkerIdTests
+    {
+        [Test]
+        public void Id_ShouldHave_KeyAttribute()
+        {
+            var obj = new Worker();
+
+            var result = obj.GetType()
+                            .GetProperty("Id")
+                            .GetCustomAttributes(false)
+                            .Where(x => x.GetType() == typeof(KeyAttribute))
+                            .Any();
+
+            Assert.IsTrue(result);
+        }
+
+        [TestCase(6)]
+        [TestCase(654353)]
+        public void Id_GetAndSetShould_WorkProperly(int randomNumber)
+        {
+            var obj = new Worker();
+
+            obj.Id = randomNumber;
+
+            Assert.AreEqual(randomNumber, obj.Id);
+        }
+    }
+}
