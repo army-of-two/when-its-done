9d7a685e3e38df6ffa6f5d8bf912dee119bff60a
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-22 12:41:37 +0200

    added WorkerReview Client Tests

---
+++ WhenItsDone/Tests/LibTests/WhenItsDone.Models.Tests/WorkerReviewTests/WorkerReviewClientTests.cs
@@ -0,0 +1,35 @@
+﻿using Moq;
+using NUnit.Framework;
+using System.Linq;
+
+namespace WhenItsDone.Models.Tests.ClientReviewTests
+{
+    [TestFixture]
+    public class WorkerReviewClientTests
+    {
+        [Test]
+        public void Client_GetAndSetShould_WorkProperly()
+        {
+            var mockedWorker = new Mock<Client>();
+
+            var obj = new WorkerReview();
+
+            obj.Client = mockedWorker.Object;
+
+            Assert.AreSame(mockedWorker.Object, obj.Client);
+        }
+
+        [Test]
+        public void Client_ShouldBe_Virtual()
+        {
+            var obj = new WorkerReview();
+
+            var result = obj.GetType()
+                            .GetProperty("Client")
+                            .GetAccessors()
+                            .Any(x => x.IsVirtual);
+
+            Assert.IsTrue(result);
+        }
+    }
+}
