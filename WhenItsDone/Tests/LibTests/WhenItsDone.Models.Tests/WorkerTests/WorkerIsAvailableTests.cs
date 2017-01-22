f4f8bda78f71656604a260b210734c61e5dd2aa0
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-22 10:14:29 +0200

    added Worker IsAvailable Tests

---
+++ WhenItsDone/Tests/LibTests/WhenItsDone.Models.Tests/WorkerTests/WorkerIsAvailableTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace WhenItsDone.Models.Tests.WorkerTests
+{
+    [TestFixture]
+    public class WorkerIsAvailableTests
+    {
+        [TestCase(true)]
+        [TestCase(false)]
+        public void IsAvailable_GetAndSetShould_WorkProperly(bool value)
+        {
+            var obj = new Worker();
+
+            obj.IsAvailable = value;
+
+            Assert.AreEqual(value, obj.IsAvailable);
+        }
+    }
+}
