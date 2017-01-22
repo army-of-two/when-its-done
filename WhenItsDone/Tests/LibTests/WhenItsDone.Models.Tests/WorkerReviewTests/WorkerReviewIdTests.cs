c339c808def2a8655de270882007719530be81f4
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-22 11:42:44 +0200

    added WorkerReview Id Tests and KeyAttribute on Id in model class

---
+++ WhenItsDone/Tests/LibTests/WhenItsDone.Models.Tests/WorkerReviewTests/WorkerReviewIdTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace WhenItsDone.Models.Tests.WorkerReviewTests
+{
+    [TestFixture]
+    public class WorkerReviewIdTests
+    {
+        [Test]
+        public void Id_ShouldHave_KeyAttribute()
+        {
+            var obj = new WorkerReview();
+
+            var result = obj.GetType()
+                            .GetProperty("Id")
+                            .GetCustomAttributes(false)
+                            .Where(x => x.GetType() == typeof(KeyAttribute))
+                            .Any();
+
+            Assert.IsTrue(result);
+        }
+
+        [TestCase(60)]
+        [TestCase(64324234)]
+        public void Id_GetAndSetShould_WorkProperly(int randomNumber)
+        {
+            var obj = new WorkerReview();
+
+            obj.Id = randomNumber;
+
+            Assert.AreEqual(randomNumber, obj.Id);
+        }
+    }
+}
