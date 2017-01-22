3959f36b667e17b9b41d9883cfd6d6b55176b760
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-22 10:15:39 +0200

    added Worker IsDeleted Tests

---
+++ WhenItsDone/Tests/LibTests/WhenItsDone.Models.Tests/WorkerTests/WorkerIsDeletedTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace WhenItsDone.Models.Tests.WorkerTests
+{
+    [TestFixture]
+    public class WorkerIsDeletedTests
+    {
+        [TestCase(true)]
+        [TestCase(false)]
+        public void IsDeleted_GetAndSetShould_WorkProperly(bool value)
+        {
+            var obj = new Worker();
+
+            obj.IsDeleted = value;
+
+            Assert.AreEqual(value, obj.IsDeleted);
+        }
+    }
+}
