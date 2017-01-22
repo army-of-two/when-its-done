113287f416d185012fffc16b16aa349e75b8d513
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-22 11:38:00 +0200

    added WorkerReview As DbModel Tests

---
+++ WhenItsDone/Tests/LibTests/WhenItsDone.Models.Tests/WorkerReviewTests/WorkerReviewAsDbModelTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System.Linq;
+using WhenItsDone.Models.Contracts;
+
+namespace WhenItsDone.Models.Tests.WorkerReviewTests
+{
+    [TestFixture]
+    public class WorkerReviewAsDbModelTests
+    {
+        [Test]
+        public void WorkerReviewClass_ShouldImplement_IDbModelInterface()
+        {
+            var obj = new WorkerReview();
+
+            var result = obj.GetType()
+                            .GetInterfaces()
+                            .Where(x => x == typeof(IDbModel))
+                            .Any();
+
+            Assert.IsTrue(result);
+        }
+    }
+}
