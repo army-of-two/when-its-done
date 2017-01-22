b0c2e099c153ad3d3b38af0753baeceee0d170dc
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-22 10:27:52 +0200

    added Worker ContactInformation Tests

---
+++ WhenItsDone/Tests/LibTests/WhenItsDone.Models.Tests/WorkerTests/WorkerContactInformationTests.cs
@@ -0,0 +1,21 @@
+﻿using Moq;
+using NUnit.Framework;
+
+namespace WhenItsDone.Models.Tests.WorkerTests
+{
+    [TestFixture]
+    public class WorkerContactInformationTests
+    {
+        [Test]
+        public void ContactInformation_GetAndSetShould_WorkProperly()
+        {
+            var mockedVS = new Mock<ContactInformation>();
+
+            var obj = new Worker();
+
+            obj.ContactInformation = mockedVS.Object;
+
+            Assert.AreSame(mockedVS.Object, obj.ContactInformation);
+        }
+    }
+}
