dade33ea9998fe70876c2f2830f58b18e8894fcd
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-22 08:45:32 +0200

    added VitalStatistics Id Tests

---
+++ WhenItsDone/Tests/LibTests/WhenItsDone.Models.Tests/VitalStatisticsTests/VitalStatisticsIdTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace WhenItsDone.Models.Tests.VitalStatisticsTests
+{
+    [TestFixture]
+    public class VitalStatisticsIdTests
+    {
+        [TestCase(5)]
+        [TestCase(7887)]
+        public void Id_GetAndSetShould_WorkProperly(int randomNumber)
+        {
+            var obj = new VitalStatistics();
+
+            obj.Id = randomNumber;
+
+            Assert.AreEqual(randomNumber, obj.Id);
+        }
+    }
+}
