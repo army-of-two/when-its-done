f0c44dae9256c2fb718abd7d20837fd1e16bc75d
shakuu <todorov.h@gmail.com>
2017-01-22 11:52:16 +0200

    added more AsyncGenericRepository.GetAllFilter tests

--- WhenItsDone/Tests/LibTests/WhenItsDone.Data.Tests/RepositoriesTests/AsyncGenericRepositoryTests/GetAllFilterTests.cs
+++ WhenItsDone/Tests/LibTests/WhenItsDone.Data.Tests/RepositoriesTests/AsyncGenericRepositoryTests/GetAllFilterTests.cs
@@ -41,9 +41,6 @@ namespace WhenItsDone.Data.Tests.RepositoriesTests.AsyncGenericRepositoryTests
             dbSetField.SetValue(asyncGenericRepositoryInstace, mockDbSet.Object);
 
             // Setup data
-            var fakeDeletedModel = new Mock<IDbModel>();
-            fakeDeletedModel.SetupGet(model => model.IsDeleted).Returns(true);
-
             var fakeData = new List<IDbModel>()
             {
                new Mock<IDbModel>().Object,
@@ -64,6 +61,139 @@ namespace WhenItsDone.Data.Tests.RepositoriesTests.AsyncGenericRepositoryTests
             var actualReturnedCollection = asyncGenericRepositoryInstace.GetAll(filter);
 
             Assert.That(actualReturnedCollection.Result.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void ShouldReturnTaskWithCorrectResult_WhenItemIsFound()
+        {
+            // This is needed to create the instance.
+            // DbContext.Set<>() returns DbSet rather than IDbSet<>.
+            var ctorParameters = new Type[] { };
+            var ctorBindingFlags = BindingFlags.NonPublic | BindingFlags.Instance;
+            var dbSetConstructor = typeof(DbSet<IDbModel>).GetConstructor(ctorBindingFlags, null, ctorParameters, null);
+            var fakeDbSet = (DbSet<IDbModel>)dbSetConstructor.Invoke(null);
+
+            var mockDbContext = new Mock<IWhenItsDoneDbContext>();
+            mockDbContext.Setup(mock => mock.Set<IDbModel>()).Returns(fakeDbSet);
+
+            var asyncGenericRepositoryInstace = new AsyncGenericRepository<IDbModel>(mockDbContext.Object);
+
+            // This is needed to mock the IDbSet<> object.
+            var mockDbSet = new Mock<IDbSet<IDbModel>>();
+            var fieldName = "dbSet";
+            var bindingFlags = BindingFlags.NonPublic | BindingFlags.Instance;
+            var dbSetField = asyncGenericRepositoryInstace.GetType().GetField(fieldName, bindingFlags);
+            dbSetField.SetValue(asyncGenericRepositoryInstace, mockDbSet.Object);
+
+            // Setup Data
+            var fakeMatchingModel = new Mock<IDbModel>();
+            fakeMatchingModel.SetupGet(model => model.Id).Returns(1);
+
+            var fakeData = new List<IDbModel>()
+            {
+                fakeMatchingModel.Object,
+                new Mock<IDbModel>().Object,
+                new Mock<IDbModel>().Object,
+                new Mock<IDbModel>().Object,
+                new Mock<IDbModel>().Object,
+                new Mock<IDbModel>().Object,
+                new Mock<IDbModel>().Object
+            }
+            .AsQueryable();
+
+            mockDbSet.As<IQueryable<IDbModel>>().Setup(m => m.Provider).Returns(fakeData.Provider);
+            mockDbSet.As<IQueryable<IDbModel>>().Setup(m => m.Expression).Returns(fakeData.Expression);
+            mockDbSet.As<IQueryable<IDbModel>>().Setup(m => m.ElementType).Returns(fakeData.ElementType);
+            mockDbSet.As<IQueryable<IDbModel>>().Setup(m => m.GetEnumerator()).Returns(fakeData.GetEnumerator());
+
+            Expression<Func<IDbModel, bool>> filter = (IDbModel model) => model.Id == 1;
+            var actualReturnedCollection = asyncGenericRepositoryInstace.GetAll(filter);
+
+            var expectedCollection = new List<IDbModel>() { fakeMatchingModel.Object };
+
+            Assert.That(actualReturnedCollection.Result, Is.Not.Null.And.EquivalentTo(expectedCollection));
+        }
+
+        [Test]
+        public void ShouldReturnTaskOfCorrectType_WhenItemIsFound()
+        {
+            // This is needed to create the instance.
+            // DbContext.Set<>() returns DbSet rather than IDbSet<>.
+            var ctorParameters = new Type[] { };
+            var ctorBindingFlags = BindingFlags.NonPublic | BindingFlags.Instance;
+            var dbSetConstructor = typeof(DbSet<IDbModel>).GetConstructor(ctorBindingFlags, null, ctorParameters, null);
+            var fakeDbSet = (DbSet<IDbModel>)dbSetConstructor.Invoke(null);
+
+            var mockDbContext = new Mock<IWhenItsDoneDbContext>();
+            mockDbContext.Setup(mock => mock.Set<IDbModel>()).Returns(fakeDbSet);
+
+            var asyncGenericRepositoryInstace = new AsyncGenericRepository<IDbModel>(mockDbContext.Object);
+
+            // This is needed to mock the IDbSet<> object.
+            var mockDbSet = new Mock<IDbSet<IDbModel>>();
+            var fieldName = "dbSet";
+            var bindingFlags = BindingFlags.NonPublic | BindingFlags.Instance;
+            var dbSetField = asyncGenericRepositoryInstace.GetType().GetField(fieldName, bindingFlags);
+            dbSetField.SetValue(asyncGenericRepositoryInstace, mockDbSet.Object);
+
+            var fakeDeletedModel = new Mock<IDbModel>();
+            fakeDeletedModel.SetupGet(model => model.IsDeleted).Returns(true);
+
+            var fakeData = new List<IDbModel>()
+            {
+               fakeDeletedModel.Object
+            }
+            .AsQueryable();
+
+            mockDbSet.As<IQueryable<IDbModel>>().Setup(m => m.Provider).Returns(fakeData.Provider);
+            mockDbSet.As<IQueryable<IDbModel>>().Setup(m => m.Expression).Returns(fakeData.Expression);
+            mockDbSet.As<IQueryable<IDbModel>>().Setup(m => m.ElementType).Returns(fakeData.ElementType);
+            mockDbSet.As<IQueryable<IDbModel>>().Setup(m => m.GetEnumerator()).Returns(fakeData.GetEnumerator());
+
+            var actualReturnedCollection = asyncGenericRepositoryInstace.GetDeleted();
+
+            Assert.That(actualReturnedCollection.GetType(), Is.EqualTo(typeof(Task<IEnumerable<IDbModel>>)));
+        }
+
+        [Test]
+        public void ShouldReturnTaskOfCorrectStatus_WhenItemIsFound()
+        {
+            // This is needed to create the instance.
+            // DbContext.Set<>() returns DbSet rather than IDbSet<>.
+            var ctorParameters = new Type[] { };
+            var ctorBindingFlags = BindingFlags.NonPublic | BindingFlags.Instance;
+            var dbSetConstructor = typeof(DbSet<IDbModel>).GetConstructor(ctorBindingFlags, null, ctorParameters, null);
+            var fakeDbSet = (DbSet<IDbModel>)dbSetConstructor.Invoke(null);
+
+            var mockDbContext = new Mock<IWhenItsDoneDbContext>();
+            mockDbContext.Setup(mock => mock.Set<IDbModel>()).Returns(fakeDbSet);
+
+            var asyncGenericRepositoryInstace = new AsyncGenericRepository<IDbModel>(mockDbContext.Object);
+
+            // This is needed to mock the IDbSet<> object.
+            var mockDbSet = new Mock<IDbSet<IDbModel>>();
+            var fieldName = "dbSet";
+            var bindingFlags = BindingFlags.NonPublic | BindingFlags.Instance;
+            var dbSetField = asyncGenericRepositoryInstace.GetType().GetField(fieldName, bindingFlags);
+            dbSetField.SetValue(asyncGenericRepositoryInstace, mockDbSet.Object);
+
+            var fakeDeletedModel = new Mock<IDbModel>();
+            fakeDeletedModel.SetupGet(model => model.IsDeleted).Returns(true);
+
+            var fakeData = new List<IDbModel>()
+            {
+               fakeDeletedModel.Object
+            }
+            .AsQueryable();
+
+            mockDbSet.As<IQueryable<IDbModel>>().Setup(m => m.Provider).Returns(fakeData.Provider);
+            mockDbSet.As<IQueryable<IDbModel>>().Setup(m => m.Expression).Returns(fakeData.Expression);
+            mockDbSet.As<IQueryable<IDbModel>>().Setup(m => m.ElementType).Returns(fakeData.ElementType);
+            mockDbSet.As<IQueryable<IDbModel>>().Setup(m => m.GetEnumerator()).Returns(fakeData.GetEnumerator());
+
+            var actualReturnedCollection = asyncGenericRepositoryInstace.GetDeleted();
+
+            Assert.That(actualReturnedCollection.Status, Is.EqualTo(TaskStatus.Running).Or.EqualTo(TaskStatus.WaitingToRun));
         }
     }
 }
