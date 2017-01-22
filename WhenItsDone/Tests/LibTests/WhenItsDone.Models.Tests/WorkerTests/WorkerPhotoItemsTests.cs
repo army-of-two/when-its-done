4f979303fb005046ed5767470cbf1df34ba69bb9
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-22 10:35:22 +0200

    added Worker PhotoItems Tests

---
+++ WhenItsDone/Tests/LibTests/WhenItsDone.Models.Tests/WorkerTests/WorkerPhotoItemsTests.cs
@@ -0,0 +1,22 @@
+﻿using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace WhenItsDone.Models.Tests.WorkerTests
+{
+    [TestFixture]
+    public class WorkerPhotoItemsTests
+    {
+        [Test]
+        public void PhotoItems_GetAndSetShould_WorkProperly()
+        {
+            var mockedCollection = new Mock<ICollection<PhotoItem>>();
+
+            var obj = new Worker();
+
+            obj.PhotoItems = mockedCollection.Object;
+
+            Assert.AreSame(mockedCollection.Object, obj.PhotoItems);
+        }
+    }
+}
