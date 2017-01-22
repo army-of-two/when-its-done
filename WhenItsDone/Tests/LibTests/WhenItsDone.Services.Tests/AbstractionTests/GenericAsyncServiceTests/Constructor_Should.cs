e710ae4ed1ae705eb8b129697d61753425eef50a
shakuu <todorov.h@gmail.com>
2017-01-22 16:05:01 +0200

    scaffolded GenericAsyncService constructor tests

---
+++ WhenItsDone/Tests/LibTests/WhenItsDone.Services.Tests/AbstractionTests/GenericAsyncServiceTests/Constructor_Should.cs
@@ -0,0 +1,13 @@
+﻿using Moq;
+using NUnit.Framework;
+
+namespace WhenItsDone.Services.Tests.AbstractionTests.GenericAsyncServiceTests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+        // should throw on null
+        // assign values
+        // implements interface
+    }
+}
