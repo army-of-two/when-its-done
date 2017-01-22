e9861e43bf55d3939c78ad5c068ef4f2c292c32d
shakuu <todorov.h@gmail.com>
2017-01-22 13:33:09 +0200

    added more AsyncGenericRepository.GetAll with filter, order by and pagination tests

--- WhenItsDone/Tests/LibTests/WhenItsDone.Data.Tests/RepositoriesTests/AsyncGenericRepositoryTests/GetAllFilterOrderByPagination_Should.cs
+++ WhenItsDone/Tests/LibTests/WhenItsDone.Data.Tests/RepositoriesTests/AsyncGenericRepositoryTests/GetAllFilterOrderByPagination_Should.cs
@@ -106,11 +106,13 @@ namespace WhenItsDone.Data.Tests.RepositoriesTests.AsyncGenericRepositoryTests
             mockDbSet.As<IQueryable<IDbModel>>().Setup(m => m.ElementType).Returns(fakeData.ElementType);
             mockDbSet.As<IQueryable<IDbModel>>().Setup(m => m.GetEnumerator()).Returns(fakeData.GetEnumerator());
 
+            var page = 0;
+            var pageSize = 5;
             Expression<Func<IDbModel, bool>> filter = (IDbModel model) => model.Id == 1;
             Expression<Func<IDbModel, int>> orderBy = null;
 
             Assert.That(
-                () => asyncGenericRepositoryInstace.GetAll(filter, orderBy),
+                () => asyncGenericRepositoryInstace.GetAll(filter, orderBy, page, pageSize),
                 Throws.InstanceOf<ArgumentNullException>().With.Message.Contains(nameof(orderBy)));
         }
 
