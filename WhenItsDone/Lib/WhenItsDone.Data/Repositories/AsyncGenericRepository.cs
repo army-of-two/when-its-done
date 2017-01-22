9ea59aa8ad5fd42804d9e804a6c406ae9dde5b7f
shakuu <todorov.h@gmail.com>
2017-01-22 13:02:16 +0200

    removed unnecessary using

--- WhenItsDone/Lib/WhenItsDone.Data/Repositories/AsyncGenericRepository.cs
+++ WhenItsDone/Lib/WhenItsDone.Data/Repositories/AsyncGenericRepository.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
-using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
