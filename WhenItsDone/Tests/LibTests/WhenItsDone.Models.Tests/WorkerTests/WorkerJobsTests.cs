636d5a56fdf2bb9bb7bb4c1eb0ecaab5fc9ffc57
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-22 10:36:34 +0200

    Added Worker Jobs Tests

---
+++ WhenItsDone/Tests/LibTests/WhenItsDone.Models.Tests/WorkerTests/WorkerJobsTests.cs
@@ -0,0 +1,22 @@
+﻿using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace WhenItsDone.Models.Tests.WorkerTests
+{
+    [TestFixture]
+    public class WorkerJobsTests
+    {
+        [Test]
+        public void Jobs_GetAndSetShould_WorkProperly()
+        {
+            var mockedCollection = new Mock<ICollection<Job>>();
+
+            var obj = new Worker();
+
+            obj.Jobs = mockedCollection.Object;
+
+            Assert.AreSame(mockedCollection.Object, obj.Jobs);
+        }
+    }
+}
