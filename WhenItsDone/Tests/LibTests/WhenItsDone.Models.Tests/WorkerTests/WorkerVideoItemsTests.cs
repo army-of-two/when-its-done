0ed6fff6b6326c1222e7d1da59f7300bd90baadd
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-22 10:33:49 +0200

    added Worker VideoItems Tests

---
+++ WhenItsDone/Tests/LibTests/WhenItsDone.Models.Tests/WorkerTests/WorkerVideoItemsTests.cs
@@ -0,0 +1,22 @@
+﻿using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace WhenItsDone.Models.Tests.WorkerTests
+{
+    [TestFixture]
+    public class WorkerVideoItemsTests
+    {
+        [Test]
+        public void VideoItems_GetAndSetShould_WorkProperly()
+        {
+            var mockedCollection = new Mock<ICollection<VideoItem>>();
+
+            var obj = new Worker();
+
+            obj.VideoItems = mockedCollection.Object;
+
+            Assert.AreSame(mockedCollection.Object, obj.VideoItems);
+        }
+    }
+}
