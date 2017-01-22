857845e9fd60347fa17359e22979679fb3dcc9bd
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-22 09:12:18 +0200

    added VitalStatistics test of tests

--- WhenItsDone/Tests/LibTests/WhenItsDone.Models.Tests/VitalStatisticsTests/VitalStatisticsIdTests.cs
+++ WhenItsDone/Tests/LibTests/WhenItsDone.Models.Tests/VitalStatisticsTests/VitalStatisticsIdTests.cs
@@ -1,10 +1,26 @@
 ﻿using NUnit.Framework;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
 
 namespace WhenItsDone.Models.Tests.VitalStatisticsTests
 {
     [TestFixture]
     public class VitalStatisticsIdTests
     {
+        [Test]
+        public void Id_ShouldHave_KeyAttribute()
+        {
+            var obj = new VitalStatistics();
+
+            var result = obj.GetType()
+                            .GetProperty("Id")
+                            .GetCustomAttributes(false)
+                            .Where(x => x.GetType() == typeof(KeyAttribute))
+                            .Any();
+
+            Assert.IsTrue(result);
+        }
+
         [TestCase(5)]
         [TestCase(7887)]
         public void Id_GetAndSetShould_WorkProperly(int randomNumber)
---
+++ WhenItsDone/Tests/LibTests/WhenItsDone.Models.Tests/VitalStatisticsTests/VitalStatisticsTestingTests.cs
@@ -0,0 +1,335 @@
+﻿using NUnit.Framework;
+using System.Linq;
+
+namespace WhenItsDone.Models.Tests.VitalStatisticsTests
+{
+    [TestFixture]
+    public class VitalStatisticsTestingTests
+    {
+        /// <summary>
+        /// At that moment VitalStatistics class have 5 tested properties .. if another property is added this test will fail
+        /// That mean new tests are REQUIRED!
+        /// </summary>
+        [Test]
+        public void VitalStatistics_VerifyNumberOfProperties()
+        {
+            var obj = new VitalStatistics();
+
+            var result = obj.GetType()
+                            .GetProperties()
+                            .Count();
+
+            Assert.AreEqual(5, result);
+        }
+
+        /// <summary>
+        /// At that moment VitalStatistics class have 1 tested constructor
+        /// </summary>
+        [Test]
+        public void VitalStatistics_VerifyNumberOfConstructors()
+        {
+            var obj = new VitalStatistics();
+
+            var methodsCount = obj.GetType()
+                                    .GetMethods()
+                                    .Count();
+
+            var propertiesCount = obj.GetType()
+                                    .GetProperties()
+                                    .Count();
+
+            var result = obj.GetType()
+                            .GetMembers()
+                            .Count();
+
+            result = result - propertiesCount - methodsCount;
+
+            Assert.AreEqual(1, result);
+        }
+
+        /// <summary>
+        /// At that moment VitalStatistics class have 0 tested methods
+        /// </summary>
+        [Test]
+        public void VitalStatistics_VerifyNumberOfMethods()
+        {
+            var methodsFromFramework = 4;
+            var expectedMethodsCount = 0;
+            var totalMethodsCount = methodsFromFramework + expectedMethodsCount;
+
+            var obj = new VitalStatistics();
+
+            var numberOfMethodsComeFromProperties = obj.GetType()
+                                                        .GetProperties()
+                                                        .Select(x => 2)
+                                                        .Sum();
+
+            var result = obj.GetType()
+                            .GetMethods()
+                            .Count();
+
+            result = result - numberOfMethodsComeFromProperties;
+
+            Assert.AreEqual(totalMethodsCount, result);
+        }
+
+        /// <summary>
+        /// At that moment TestClas contain 2 Test methods = fail mean someone changed it
+        /// </summary>
+        [Test]
+        public void VitalStatisticsIdTests_VerifyNumberOfTests()
+        {
+            var methodsFromFramework = 4;
+            var expectedMethods = 2;
+            var totalExpectedMethods = methodsFromFramework + expectedMethods;
+
+            var obj = new VitalStatisticsIdTests();
+
+            var result = obj.GetType()
+                            .GetMethods()
+                            .Count();
+
+            Assert.AreEqual(totalExpectedMethods, result);
+        }
+
+        /// <summary>
+        /// At that moment TestClass contains 2 TestCase attributes - fail mean someone changed it
+        /// </summary>
+        [Test]
+        public void VitalStatisticsIdTests_VeryfyTestCaseAttributes()
+        {
+            var obj = new VitalStatisticsIdTests();
+
+            var result = obj.GetType()
+                            .GetMethods()
+                            .Select(x => x.GetCustomAttributes(false)
+                                        .Where(z => z.GetType() == typeof(TestCaseAttribute))
+                                        .Count())
+                            .Sum();
+
+            Assert.AreEqual(2, result);
+        }
+
+        /// <summary>
+        /// At that moment TestClas contain 1 Test method = fail mean someone changed it
+        /// </summary>
+        [Test]
+        public void VitalStatisticsIsDeletedTests_VerifyNumberOfTests()
+        {
+            var methodsFromFramework = 4;
+            var expectedMethods = 1;
+            var totalExpectedMethods = methodsFromFramework + expectedMethods;
+
+            var obj = new VitalStatisticsIsDeletedTests();
+
+            var result = obj.GetType()
+                            .GetMethods()
+                            .Count();
+
+            Assert.AreEqual(totalExpectedMethods, result);
+        }
+
+        /// <summary>
+        /// At that moment TestClass contains 2 TestCase attributes - fail mean someone changed it
+        /// </summary>
+        [Test]
+        public void VitalStatisticsIsDeletedTests_VeryfyTestCaseAttributes()
+        {
+            var obj = new VitalStatisticsIsDeletedTests();
+
+            var result = obj.GetType()
+                            .GetMethods()
+                            .Select(x => x.GetCustomAttributes(false)
+                                        .Where(z => z.GetType() == typeof(TestCaseAttribute))
+                                        .Count())
+                            .Sum();
+
+            Assert.AreEqual(2, result);
+        }
+
+        /// <summary>
+        /// At that moment TestClas contain 1 Test method = fail mean someone changed it
+        /// </summary>
+        [Test]
+        public void VitalStatisticsAsDbModelTests_VerifyNumberOfTests()
+        {
+            var methodsFromFramework = 4;
+            var expectedMethods = 1;
+            var totalExpectedMethods = methodsFromFramework + expectedMethods;
+
+            var obj = new VitalStatisticsAsDbModelTests();
+
+            var result = obj.GetType()
+                            .GetMethods()
+                            .Count();
+
+            Assert.AreEqual(totalExpectedMethods, result);
+        }
+
+        /// <summary>
+        /// At that moment TestClass contains 0 TestCase attributes - fail mean someone changed it
+        /// </summary>
+        [Test]
+        public void VitalStatisticsAsDbModelTests_VeryfyTestCaseAttributes()
+        {
+            var obj = new VitalStatisticsAsDbModelTests();
+
+            var result = obj.GetType()
+                            .GetMethods()
+                            .Select(x => x.GetCustomAttributes(false)
+                                        .Where(z => z.GetType() == typeof(TestCaseAttribute))
+                                        .Count())
+                            .Sum();
+
+            Assert.AreEqual(0, result);
+        }
+
+        /// <summary>
+        /// At that moment TestClas contain 6 Test methods = fail mean someone changed it
+        /// </summary>
+        [Test]
+        public void VitalStatisticsConstructorTests_VerifyNumberOfTests()
+        {
+            var methodsFromFramework = 4;
+            var expectedMethods = 6;
+            var totalExpectedMethods = methodsFromFramework + expectedMethods;
+
+            var obj = new VitalStatisticsConstructorTests();
+
+            var result = obj.GetType()
+                            .GetMethods()
+                            .Count();
+
+            Assert.AreEqual(totalExpectedMethods, result);
+        }
+
+        /// <summary>
+        /// At that moment TestClass contains 0 TestCase attributes - fail mean someone changed it
+        /// </summary>
+        [Test]
+        public void VitalStatisticsConstructorTests_VeryfyTestCaseAttributes()
+        {
+            var obj = new VitalStatisticsConstructorTests();
+
+            var result = obj.GetType()
+                            .GetMethods()
+                            .Select(x => x.GetCustomAttributes(false)
+                                        .Where(z => z.GetType() == typeof(TestCaseAttribute))
+                                        .Count())
+                            .Sum();
+
+            Assert.AreEqual(0, result);
+        }
+
+        /// <summary>
+        /// At that moment TestClas contain 4 Test methods = fail mean someone changed it
+        /// </summary>
+        [Test]
+        public void VitalStatisticsBustSizeInCmTests_VerifyNumberOfTests()
+        {
+            var methodsFromFramework = 4;
+            var expectedMethods = 4;
+            var totalExpectedMethods = methodsFromFramework + expectedMethods;
+
+            var obj = new VitalStatisticsBustSizeInCmTests();
+
+            var result = obj.GetType()
+                            .GetMethods()
+                            .Count();
+
+            Assert.AreEqual(totalExpectedMethods, result);
+        }
+
+        /// <summary>
+        /// At that moment TestClass contains 2 TestCase attributes - fail mean someone changed it
+        /// </summary>
+        [Test]
+        public void VitalStatisticsBustSizeInCmTests_VeryfyTestCaseAttributes()
+        {
+            var obj = new VitalStatisticsBustSizeInCmTests();
+
+            var result = obj.GetType()
+                            .GetMethods()
+                            .Select(x => x.GetCustomAttributes(false)
+                                        .Where(z => z.GetType() == typeof(TestCaseAttribute))
+                                        .Count())
+                            .Sum();
+
+            Assert.AreEqual(2, result);
+        }
+
+        /// <summary>
+        /// At that moment TestClas contain 4 Test methods = fail mean someone changed it
+        /// </summary>
+        [Test]
+        public void VitalStatisticsHipSizeInCmTests_VerifyNumberOfTests()
+        {
+            var methodsFromFramework = 4;
+            var expectedMethods = 4;
+            var totalExpectedMethods = methodsFromFramework + expectedMethods;
+
+            var obj = new VitalStatisticsHipSizeInCmTests();
+
+            var result = obj.GetType()
+                            .GetMethods()
+                            .Count();
+
+            Assert.AreEqual(totalExpectedMethods, result);
+        }
+
+        /// <summary>
+        /// At that moment TestClass contains 2 TestCase attributes - fail mean someone changed it
+        /// </summary>
+        [Test]
+        public void VitalStatisticsHipSizeInCmTests_VeryfyTestCaseAttributes()
+        {
+            var obj = new VitalStatisticsHipSizeInCmTests();
+
+            var result = obj.GetType()
+                            .GetMethods()
+                            .Select(x => x.GetCustomAttributes(false)
+                                        .Where(z => z.GetType() == typeof(TestCaseAttribute))
+                                        .Count())
+                            .Sum();
+
+            Assert.AreEqual(2, result);
+        }
+
+        /// <summary>
+        /// At that moment TestClas contain 4 Test methods = fail mean someone changed it
+        /// </summary>
+        [Test]
+        public void VitalStatisticsWaistSizeInCmTests_VerifyNumberOfTests()
+        {
+            var methodsFromFramework = 4;
+            var expectedMethods = 4;
+            var totalExpectedMethods = methodsFromFramework + expectedMethods;
+
+            var obj = new VitalStatisticsWaistSizeInCmTests();
+
+            var result = obj.GetType()
+                            .GetMethods()
+                            .Count();
+
+            Assert.AreEqual(totalExpectedMethods, result);
+        }
+
+        /// <summary>
+        /// At that moment TestClass contains 2 TestCase attributes - fail mean someone changed it
+        /// </summary>
+        [Test]
+        public void VitalStatisticsWaistSizeInCmTests_VeryfyTestCaseAttributes()
+        {
+            var obj = new VitalStatisticsWaistSizeInCmTests();
+
+            var result = obj.GetType()
+                            .GetMethods()
+                            .Select(x => x.GetCustomAttributes(false)
+                                        .Where(z => z.GetType() == typeof(TestCaseAttribute))
+                                        .Count())
+                            .Sum();
+
+            Assert.AreEqual(2, result);
+        }
+    }
+}
