17033ac767ed9874f8b430fb6b24a91968a26724
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-22 18:05:52 +0200

    added Stateful class

---
+++ WhenItsDone/Lib/WhenItsDone.Data/Adapters/StatefulEntry.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using WhenItsDone.Data.Contracts;
+
+namespace WhenItsDone.Data.Adapters
+{
+    public class Stateful<T> : IStateful<T>
+    {
+        private readonly DbEntityEntry entry;
+
+        public Stateful(DbEntityEntry entry)
+        {
+            if (entry == null)
+            {
+                throw new ArgumentNullException("Entry");
+            }
+
+            this.entry = entry;
+        }
+
+        public EntityState State
+        {
+            get
+            {
+                return this.entry.State;
+            }
+
+            set
+            {
+                this.entry.State = value;
+            }
+        }
+    }
+}
