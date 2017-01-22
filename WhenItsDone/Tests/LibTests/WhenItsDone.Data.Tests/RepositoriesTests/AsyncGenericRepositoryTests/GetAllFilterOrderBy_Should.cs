ba5b852c9a8083255ebc901284edb10d651d3d6c
shakuu <todorov.h@gmail.com>
2017-01-22 12:03:58 +0200

    added more AsyncGenericRepository.GetAll with filter and oreder by tests

--- WhenItsDone/Tests/LibTests/WhenItsDone.Data.Tests/RepositoriesTests/AsyncGenericRepositoryTests/GetAllFilterOrderBy_Should.cs
+++ WhenItsDone/Tests/LibTests/WhenItsDone.Data.Tests/RepositoriesTests/AsyncGenericRepositoryTests/GetAllFilterOrderBy_Should.cs
@@ -162,7 +162,9 @@ namespace WhenItsDone.Data.Tests.RepositoriesTests.AsyncGenericRepositoryTests
             mockDbSet.As<IQueryable<IDbModel>>().Setup(m => m.GetEnumerator()).Returns(fakeData.GetEnumerator());
 
             Expression<Func<IDbModel, bool>> filter = (IDbModel model) => model.Id == 1;
-            var actualReturnedCollection = asyncGenericRepositoryInstace.GetAll(filter);
+            Expression<Func<IDbModel, int>> orderBy = (IDbModel model) => model.Id;
+
+            var actualReturnedCollection = asyncGenericRepositoryInstace.GetAll(filter, orderBy);
 
             Assert.That(actualReturnedCollection.GetType(), Is.EqualTo(typeof(Task<IEnumerable<IDbModel>>)));
         }
