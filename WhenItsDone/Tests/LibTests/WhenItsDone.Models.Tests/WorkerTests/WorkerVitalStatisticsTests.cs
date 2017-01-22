a0152fa061efbe1144153bd17b5accfe2e59cf36
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-22 10:20:05 +0200

    added Worker VitalStatistics Tests

---
+++ WhenItsDone/Tests/LibTests/WhenItsDone.Models.Tests/WorkerTests/WorkerVitalStatisticsTests.cs
@@ -0,0 +1,21 @@
+﻿using Moq;
+using NUnit.Framework;
+
+namespace WhenItsDone.Models.Tests.WorkerTests
+{
+    [TestFixture]
+    public class WorkerVitalStatisticsTests
+    {
+        [Test]
+        public void VitalStatistics_GetAndSetShould_WorkProperly()
+        {
+            var mockedVS = new Mock<VitalStatistics>();
+
+            var obj = new Worker();
+
+            obj.VitalStatistics = mockedVS.Object;
+
+            Assert.AreSame(mockedVS.Object, obj.VitalStatistics);
+        }
+    }
+}
