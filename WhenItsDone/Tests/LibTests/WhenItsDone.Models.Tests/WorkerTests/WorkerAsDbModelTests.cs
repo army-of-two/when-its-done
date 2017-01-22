4257f7fa1d2d60c948c4eeabfeadd13076db0a57
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-22 09:18:11 +0200

    added Worker As DbModel Tests

---
+++ WhenItsDone/Tests/LibTests/WhenItsDone.Models.Tests/WorkerTests/WorkerAsDbModelTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System.Linq;
+using WhenItsDone.Models.Contracts;
+
+namespace WhenItsDone.Models.Tests.WorkerTests
+{
+    [TestFixture]
+    public class WorkerAsDbModelTests
+    {
+        [Test]
+        public void Worker_ShouldImplement_IDbModelInterface()
+        {
+            var obj = new Worker();
+
+            var result = obj.GetType()
+                            .GetInterfaces()
+                            .Where(x => x == typeof(IDbModel))
+                            .Any();
+
+            Assert.IsTrue(result);
+        }
+    }
+}
