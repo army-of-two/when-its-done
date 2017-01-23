30aa7af4cc5f32238268d80bec2e710548b6a2d8
shakuu <todorov.h@gmail.com>
2017-01-23 03:24:51 +0200

    suggested structure

---
+++ WhenItsDone/Lib/WhenItsDone.MVP/Presenters/Home/HomePresenter.cs
@@ -0,0 +1,14 @@
+﻿using WebFormsMvp;
+
+using WhenItsDone.MVP.Views.Home;
+
+namespace WhenItsDone.MVP.Presenters.Home
+{
+    public class HomePresenter : Presenter<IHomeView>
+    {
+        public HomePresenter(IHomeView view)
+            : base(view)
+        {
+        }
+    }
+}
---
+++ WhenItsDone/Lib/WhenItsDone.MVP/Views/Home/IHomeView.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using WebFormsMvp;
+
+using WhenItsDone.MVP.Models.Home;
+
+namespace WhenItsDone.MVP.Views.Home
+{
+    public interface IHomeView : IView<HomeViewModel>
+    {
+        event EventHandler MoreInformation;
+    }
+}
