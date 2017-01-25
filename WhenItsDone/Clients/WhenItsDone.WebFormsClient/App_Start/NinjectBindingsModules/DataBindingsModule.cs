2a7f600bc1c92a873ac8ce108d51eff6cae542de
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-25 10:51:27 +0200

    added binding for IStateful factory

--- WhenItsDone/Clients/WhenItsDone.WebFormsClient/App_Start/NinjectBindingsModules/DataBindingsModule.cs
+++ WhenItsDone/Clients/WhenItsDone.WebFormsClient/App_Start/NinjectBindingsModules/DataBindingsModule.cs
@@ -9,6 +9,9 @@ using WhenItsDone.Services;
 using WhenItsDone.Data.UnitsOfWork.Factories;
 using WhenItsDone.Data.UnitsOfWork;
 using WhenItsDone.Data;
+using WhenItsDone.Data.Factories;
+using System.Linq;
+using Ninject;
 
 namespace WhenItsDone.WebFormsClient.App_Start.NinjectBindingsModules
 {
@@ -30,6 +33,22 @@ namespace WhenItsDone.WebFormsClient.App_Start.NinjectBindingsModules
             this.Bind<IWhenItsDoneDbContext>()
                 .To<WhenItsDoneDbContext>()
                 .InRequestScope();
+
+            this.Bind<IStatefulFactory>().ToFactory().InSingletonScope();
+
+            // if binding above do not work change with code below maybe
+
+            //this.Bind(typeof(IStateful<>)).ToMethod(ctx =>
+            //{
+            //    var param = ctx.Parameters.Single();
+
+            //    var result = ctx.Kernel.Get(typeof(IStateful<>), param);
+
+            //    return result;
+            //})
+            //.InSingletonScope()
+            //// GetStateful<object> ?!?
+            //.NamedLikeFactoryMethod((IStatefulFactory fac) => fac.GetStateful<object>(null));
         }
     }
 }
