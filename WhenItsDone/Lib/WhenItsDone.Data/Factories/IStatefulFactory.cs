efd4f088053e66ad5aa273e1e76bd0faf222b475
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-25 10:13:33 +0200

    added IStateful factory interface

---
+++ WhenItsDone/Lib/WhenItsDone.Data/Factories/IStatefulFactory.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity.Infrastructure;
+using WhenItsDone.Data.Contracts;
+
+namespace WhenItsDone.Data.Factories
+{
+    public interface IStatefulFactory
+    {
+        IStateful<T> GetStateful<T>(DbEntityEntry<T> entry) where T : class;
+    }
+}
