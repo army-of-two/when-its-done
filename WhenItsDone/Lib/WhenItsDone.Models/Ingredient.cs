73a485ccfbbeda72ec63c03dadef72ba6cfd989a
shakuu <todorov.h@gmail.com>
2017-01-27 00:41:08 +0200

    added Recipe Model

--- WhenItsDone/Lib/WhenItsDone.Models/Ingredient.cs
+++ WhenItsDone/Lib/WhenItsDone.Models/Ingredient.cs
@@ -1,16 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using WhenItsDone.Models.Enums;
-
-namespace WhenItsDone.Models
+﻿namespace WhenItsDone.Models
 {
     public class Ingredient
     {
         public int Id { get; set; }
-        
 
+        public int FoodId { get; set; }
+
+        public Food Food { get; set; }
+
+        public decimal Quantity { get; set; }
     }
 }
---
+++ WhenItsDone/Lib/WhenItsDone.Models/Recipe.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace WhenItsDone.Models
+{
+    public class Recipe
+    {
+        private ICollection<Ingredient> ingredients;
+
+        public Recipe()
+        {
+            this.ingredients = new HashSet<Ingredient>();
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public virtual ICollection<Ingredient> Ingredients
+        {
+            get
+            {
+                return this.ingredients;
+            }
+
+            set
+            {
+                this.ingredients = value;
+            }
+        }
+    }
+}
