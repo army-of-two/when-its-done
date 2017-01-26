920be98fc1afb40cb2a66bec6c38d06b6d94423a
shakuu <todorov.h@gmail.com>
2017-01-27 00:01:05 +0200

    added Food

---
+++ WhenItsDone/Lib/WhenItsDone.Models/Food.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WhenItsDone.Models.Enums;
+
+namespace WhenItsDone.Models
+{
+    public class Food
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public IngredientType IngredientType { get; set; }
+
+        public MeasurementUnitType MeasurementUnitType { get; set; }
+
+        public decimal PricePerUnit { get; set; }
+    }
+}
---
+++ WhenItsDone/Lib/WhenItsDone.Models/Ingredient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WhenItsDone.Models.Enums;
+
+namespace WhenItsDone.Models
+{
+    public class Ingredient
+    {
+        public int Id { get; set; }
+        
+
+    }
+}
