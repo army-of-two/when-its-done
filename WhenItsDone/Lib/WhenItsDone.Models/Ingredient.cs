cb381ea24556d05af62f62461df4d051a8cb519f
shakuu <todorov.h@gmail.com>
2017-01-27 01:58:36 +0200

    added Ingredient model validation

--- WhenItsDone/Lib/WhenItsDone.Models/Ingredient.cs
+++ WhenItsDone/Lib/WhenItsDone.Models/Ingredient.cs
@@ -1,13 +1,20 @@
-﻿namespace WhenItsDone.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+using WhenItsDone.Models.Constants;
+
+namespace WhenItsDone.Models
 {
     public class Ingredient
     {
+        [Key]
         public int Id { get; set; }
 
         public int FoodId { get; set; }
 
+        [Required]
         public Food Food { get; set; }
 
+        [Range(ValidationConstants.VitaminQuantityMinValue, ValidationConstants.VitaminQuantityMaxValue)]
         public decimal Quantity { get; set; }
     }
 }
