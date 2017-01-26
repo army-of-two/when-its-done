ab814e8a7fe1e22d4ac144423486f782eeadbf31
shakuu <todorov.h@gmail.com>
2017-01-27 01:57:35 +0200

    added Mineral model validation

--- WhenItsDone/Lib/WhenItsDone.Models/Mineral.cs
+++ WhenItsDone/Lib/WhenItsDone.Models/Mineral.cs
@@ -1,11 +1,21 @@
-﻿namespace WhenItsDone.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+using WhenItsDone.Models.Constants;
+
+namespace WhenItsDone.Models
 {
     public class Mineral
     {
+        [Key]
         public int Id { get; set; }
 
+        [Range(ValidationConstants.VitaminQuantityMinValue, ValidationConstants.VitaminQuantityMaxValue)]
         public decimal Quantity { get; set; }
 
+        [Required]
+        [MinLength(ValidationConstants.NameMinLength)]
+        [MaxLength(ValidationConstants.NameMaxLength)]
+        [RegularExpression(RegexConstants.EnBgSpaceMinus)]
         public decimal Name { get; set; }
     }
 }
