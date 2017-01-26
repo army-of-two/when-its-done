a17da7e48ee70e8a9b7d52c3b3fb568b93ae3357
shakuu <todorov.h@gmail.com>
2017-01-27 00:04:01 +0200

    added NutritionFacts

---
+++ WhenItsDone/Lib/WhenItsDone.Models/NutritionFacts.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhenItsDone.Models
+{
+    public class NutritionFacts
+    {
+        public int Id { get; set; }
+
+        public decimal Calories { get; set; }
+
+        public decimal Carbohydrates { get; set; }
+
+        public decimal Fats { get; set; }
+
+        public decimal Protein { get; set; }
+
+        
+    }
+}
