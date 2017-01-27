f7d88f6c3a95019a5a1d8269455ea0c5dbd3b439
shakuu <todorov.h@gmail.com>
2017-01-27 02:00:19 +0200

    added Food Model validation

--- WhenItsDone/Lib/WhenItsDone.Models/Food.cs
+++ WhenItsDone/Lib/WhenItsDone.Models/Food.cs
@@ -1,26 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+
+using WhenItsDone.Models.Constants;
 using WhenItsDone.Models.Enums;
 
 namespace WhenItsDone.Models
 {
     public class Food
     {
+        [Key]
         public int Id { get; set; }
 
+        [Required]
+        [MinLength(ValidationConstants.NameMinLength)]
+        [MaxLength(ValidationConstants.NameMaxLength)]
+        [RegularExpression(RegexConstants.EnBgSpaceMinus)]
         public string Name { get; set; }
 
         public IngredientType IngredientType { get; set; }
 
         public MeasurementUnitType MeasurementUnitType { get; set; }
 
+        [Range(ValidationConstants.DishPriceMinValue, ValidationConstants.DishPriceMaxValue)]
         public decimal PricePerUnit { get; set; }
 
         public int NutritionFactsId { get; set; }
 
+        [Required]
         public virtual NutritionFacts NutritionFacts { get; set; }
     }
 }
