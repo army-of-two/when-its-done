87d51b8b7a2f0952c76fed78e3b265d2ebf3a73a
shakuu <todorov.h@gmail.com>
2017-01-30 14:52:59 +0200

    add DishesAsyncRepository ctor tests

---
+++ WhenItsDone/Tests/LibTests/WhenItsDone.Data.Tests/RepositoriesTests/DishesAsyncRepositoryTests/Constructor_Should.cs
@@ -0,0 +1,28 @@
+﻿using System.Data.Entity;
+
+using Moq;
+using NUnit.Framework;
+
+using WhenItsDone.Data.Contracts;
+using WhenItsDone.Data.Repositories;
+using WhenItsDone.Models.Contracts;
+using WhenItsDone.Models;
+
+namespace WhenItsDone.Data.Tests.RepositoriesTests.DishesAsyncRepositoryTests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+        [Test]
+        public void ShouldCreateAnObjectWhichImplementsIAsyncRepository()
+        {
+            var fakeDbSet = new Mock<DbSet<Dish>>();
+            var mockDbContext = new Mock<IWhenItsDoneDbContext>();
+            mockDbContext.Setup(mock => mock.Set<Dish>()).Returns(fakeDbSet.Object);
+
+            var asyncGenericRepository = new DishesAsyncRepository(mockDbContext.Object);
+
+            Assert.That(asyncGenericRepository, Is.InstanceOf<IDishesAsyncRepository>());
+        }
+    }
+}
---
+++ WhenItsDone/Tests/LibTests/WhenItsDone.Data.Tests/RepositoriesTests/DishesAsyncRepositoryTests/GetTopCountDishesByRating_Should.cs
@@ -0,0 +1,10 @@
+﻿using Moq;
+using NUnit.Framework;
+
+namespace WhenItsDone.Data.Tests.RepositoriesTests.DishesAsyncRepositoryTests
+{
+    [TestFixture]
+    public class GetTopCountDishesByRating_Should
+    {
+    }
+}
