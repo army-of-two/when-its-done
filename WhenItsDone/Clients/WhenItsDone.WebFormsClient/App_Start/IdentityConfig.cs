e5b342c20323d9d52c7c9d8b028edc33b9c53de0
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-30 10:07:11 +0200

    moved EmailService

--- WhenItsDone/Clients/WhenItsDone.WebFormsClient/App_Start/IdentityConfig.cs
+++ WhenItsDone/Clients/WhenItsDone.WebFormsClient/App_Start/IdentityConfig.cs
@@ -10,14 +10,7 @@ using WhenItsDone.WebFormsClient.Models;
 
 namespace WhenItsDone.WebFormsClient
 {
-    public class EmailService : IIdentityMessageService
-    {
-        public Task SendAsync(IdentityMessage message)
-        {
-            // Plug in your email service here to send an email.
-            return Task.FromResult(0);
-        }
-    }
+    
 
     public class SmsService : IIdentityMessageService
     {
---
+++ WhenItsDone/Lib/WhenItsDone.DefaultAuth/EmailService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhenItsDone.DefaultAuth
+{
+    public class EmailService : IIdentityMessageService
+    {
+        public Task SendAsync(IdentityMessage message)
+        {
+            // Plug in your email service here to send an email.
+            return Task.FromResult(0);
+        }
+    }
+}
