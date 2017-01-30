5e0f151119474f7b5e0f40e88c4ad7eb11fa030b
shakuu <todorov.h@gmail.com>
2017-01-30 10:18:53 +0200

    added DishesDataService

---
+++ WhenItsDone/Lib/WhenItsDone.Services/Contracts/IDishesDataService.cs
@@ -0,0 +1,8 @@
+﻿using WhenItsDone.Models;
+
+namespace WhenItsDone.Services.Contracts
+{
+    public interface IDishesDataService : IGenericAsyncService<Dish>
+    {
+    }
+}
---
+++ WhenItsDone/Lib/WhenItsDone.Services/DIshesDataService.cs
@@ -0,0 +1,16 @@
+﻿using WhenItsDone.Data.Contracts;
+using WhenItsDone.Data.UnitsOfWork.Factories;
+using WhenItsDone.Models;
+using WhenItsDone.Services.Abstraction;
+using WhenItsDone.Services.Contracts;
+
+namespace WhenItsDone.Services
+{
+    public class DishesDataService : GenericAsyncService<Dish>, IDishesDataService, IGenericAsyncService<Dish>
+    {
+        public DishesDataService(IAsyncRepository<Dish> asyncRepository, IDisposableUnitOfWorkFactory unitOfWorkFactory)
+            : base(asyncRepository, unitOfWorkFactory)
+        {
+        }
+    }
+}
