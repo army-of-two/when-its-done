6e8c492d70a0be09cc20c1d3a46487d07b363796
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-30 10:05:02 +0200

    moved ApplicationDbContext

--- WhenItsDone/Clients/WhenItsDone.WebFormsClient/Models/IdentityModels.cs
+++ WhenItsDone/Clients/WhenItsDone.WebFormsClient/Models/IdentityModels.cs
@@ -27,18 +27,7 @@ namespace WhenItsDone.WebFormsClient.Models
         }
     }
 
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
-        public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
-        {
-        }
-
-        public static ApplicationDbContext Create()
-        {
-            return new ApplicationDbContext();
-        }
-    }
+   
 }
 
 #region Helpers
---
+++ WhenItsDone/Lib/WhenItsDone.DefaultAuth/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhenItsDone.DefaultAuth
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
