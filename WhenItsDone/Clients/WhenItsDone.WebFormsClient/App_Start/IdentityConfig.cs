e61b6c805d14b737362f12d6b16d3a716e7edb7f
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-30 10:08:18 +0200

    moved SmsService

--- WhenItsDone/Clients/WhenItsDone.WebFormsClient/App_Start/IdentityConfig.cs
+++ WhenItsDone/Clients/WhenItsDone.WebFormsClient/App_Start/IdentityConfig.cs
@@ -12,14 +12,7 @@ namespace WhenItsDone.WebFormsClient
 {
     
 
-    public class SmsService : IIdentityMessageService
-    {
-        public Task SendAsync(IdentityMessage message)
-        {
-            // Plug in your SMS service here to send a text message.
-            return Task.FromResult(0);
-        }
-    }
+    
 
     // Configure the application user manager used in this application. UserManager is defined in ASP.NET Identity and is used by the application.
     public class ApplicationUserManager : UserManager<ApplicationUser>
---
+++ WhenItsDone/Lib/WhenItsDone.DefaultAuth/SmsService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhenItsDone.DefaultAuth
+{
+    public class SmsService : IIdentityMessageService
+    {
+        public Task SendAsync(IdentityMessage message)
+        {
+            // Plug in your SMS service here to send a text message.
+            return Task.FromResult(0);
+        }
+    }
+}
