0893076dd05327fd53385dc18812d8920a301a62
shakuu <todorov.h@gmail.com>
2017-01-30 11:51:47 +0200

    added TopDIshesEventArgs, TopDishesViewModel

--- WhenItsDone/Lib/WhenItsDone.MVP/ContentContainers/TopDishesMVP/TopDishesEventArgs.cs
+++ WhenItsDone/Lib/WhenItsDone.MVP/ContentContainers/TopDishesMVP/TopDishesEventArgs.cs
@@ -4,6 +4,11 @@ namespace WhenItsDone.MVP.ContentContainers.TopDishesMVP
 {
     public class TopDishesEventArgs : EventArgs
     {
+        public TopDishesEventArgs(int dishesCount)
+        {
+            this.dishesCount = dishesCount;
+        }
+
         public int dishesCount { get; set; }
     }
 }
