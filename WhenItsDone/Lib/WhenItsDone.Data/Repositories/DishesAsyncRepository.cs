bf002c1b6ebe21fbc1572ff46800ad85e42992e6
shakuu <todorov.h@gmail.com>
2017-01-30 12:35:51 +0200

    added sampleNamePhotoDishViewData to DishesAsyncRepository

--- WhenItsDone/Lib/WhenItsDone.Data/Repositories/DishesAsyncRepository.cs
+++ WhenItsDone/Lib/WhenItsDone.Data/Repositories/DishesAsyncRepository.cs
@@ -14,6 +14,8 @@ namespace WhenItsDone.Data.Repositories
 {
     public class DishesAsyncRepository : GenericAsyncRepository<Dish>, IAsyncRepository<Dish>, IDishesAsyncRepository
     {
+        private IEnumerable<NamePhotoDishView> sampleNamePhotoDishViewData;
+
         public DishesAsyncRepository(IWhenItsDoneDbContext dbContext)
             : base(dbContext)
         {
@@ -43,7 +45,29 @@ namespace WhenItsDone.Data.Repositories
 
         private IEnumerable<NamePhotoDishView> GetSampleDataOnFailedDBConnection()
         {
-            return new NamePhotoDishView[] { };
+            if (this.sampleNamePhotoDishViewData == null)
+            {
+                this.sampleNamePhotoDishViewData = this.CreateSampleNamePhotoDishViewData();
+            }
+
+            return this.sampleNamePhotoDishViewData;
+        }
+
+        private IEnumerable<NamePhotoDishView> CreateSampleNamePhotoDishViewData()
+        {
+            var modelA = new NamePhotoDishView();
+            modelA.Name = "Pepperoni";
+            modelA.PhotoItemUrl = "https://www.mmfoodmarket.com/images/default-source/products/prepared-meals/pepperoni-pizza.jpg?sfvrsn=2";
+
+            var modelB = new NamePhotoDishView();
+            modelB.Name = "Frittata";
+            modelB.PhotoItemUrl = "http://assets.epicurious.com/photos/5719486194956b31172ec2d0/master/pass/241324_hires.jpg";
+
+            var modelC = new NamePhotoDishView();
+            modelC.Name = "Steak";
+            modelC.PhotoItemUrl = "http://www.omahasteaks.com/gifs/990x594/z_fi001.jpg";
+
+            return new NamePhotoDishView[] { modelA, modelB, modelC };
         }
     }
 }
