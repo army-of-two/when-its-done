d1f8f87e9a7a96bf05101b36ad5c3357832fbc19
shakuu <todorov.h@gmail.com>
2017-01-30 10:55:44 +0200

    added Top 3 Method to service

--- WhenItsDone/Lib/WhenItsDone.Services/Contracts/IDishesDataService.cs
+++ WhenItsDone/Lib/WhenItsDone.Services/Contracts/IDishesDataService.cs
@@ -1,8 +1,12 @@
-﻿using WhenItsDone.Models;
+﻿using System.Collections.Generic;
+
+using WhenItsDone.DTOs.DishViews;
+using WhenItsDone.Models;
 
 namespace WhenItsDone.Services.Contracts
 {
     public interface IDishesDataService : IGenericAsyncService<Dish>
     {
+        IEnumerable<NamePhotoDishView> GetTopThreeDishesByRating();
     }
 }
--- WhenItsDone/Lib/WhenItsDone.Services/DIshesDataService.cs
+++ WhenItsDone/Lib/WhenItsDone.Services/DIshesDataService.cs
@@ -1,5 +1,9 @@
-﻿using WhenItsDone.Data.Contracts;
+﻿using System;
+using System.Collections.Generic;
+
+using WhenItsDone.Data.Contracts;
 using WhenItsDone.Data.UnitsOfWork.Factories;
+using WhenItsDone.DTOs.DishViews;
 using WhenItsDone.Models;
 using WhenItsDone.Services.Abstraction;
 using WhenItsDone.Services.Contracts;
@@ -8,9 +12,31 @@ namespace WhenItsDone.Services
 {
     public class DishesDataService : GenericAsyncService<Dish>, IDishesDataService, IGenericAsyncService<Dish>
     {
+        private readonly IAsyncRepository<Dish> asyncRepository;
+        private readonly IDisposableUnitOfWorkFactory unitOfWorkFactory;
+
         public DishesDataService(IAsyncRepository<Dish> asyncRepository, IDisposableUnitOfWorkFactory unitOfWorkFactory)
             : base(asyncRepository, unitOfWorkFactory)
         {
+            if (asyncRepository == null)
+            {
+                throw new ArgumentNullException(nameof(asyncRepository));
+            }
+
+            if (unitOfWorkFactory == null)
+            {
+                throw new ArgumentNullException(nameof(unitOfWorkFactory));
+            }
+
+            this.asyncRepository = asyncRepository;
+            this.unitOfWorkFactory = unitOfWorkFactory;
+        }
+
+        public IEnumerable<NamePhotoDishView> GetTopThreeDishesByRating()
+        {
+           
+
+            throw new NotImplementedException();
         }
     }
 }
