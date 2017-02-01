d25253f311b9dae6f3f2e9a51a5a513359462042
shakuu <todorov.h@gmail.com>
2017-02-01 02:42:04 +0200

    added more TopDishesPresenter tests

--- WhenItsDone/Lib/WhenItsDone.MVP/ContentContainers/TopDishesMVP/TopDishesPresenter.cs
+++ WhenItsDone/Lib/WhenItsDone.MVP/ContentContainers/TopDishesMVP/TopDishesPresenter.cs
@@ -25,6 +25,11 @@ namespace WhenItsDone.MVP.ContentContainers.TopDishesMVP
 
         public void OnGetTopDishes(object sender, TopDishesEventArgs args)
         {
+            if (args.dishesCount < 0)
+            {
+                throw new ArgumentException("dishesCount parameter must be greater than or equal to 0.");
+            }
+
             this.View.Model.TopDishes = this.dishesService.GetTopCountDishesByRating(args.dishesCount, args.AddSampleData);
         }
     }
