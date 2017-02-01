9a8f1e0a411322a73233e810a861b8dd6d51def9
Stoiko Neykov <stoikoneykov@gmail.com>
2017-02-01 15:44:43 +0200

    update IWorkersAsyncService

--- WhenItsDone/Lib/WhenItsDone.Services/Contracts/IWorkersAsyncService.cs
+++ WhenItsDone/Lib/WhenItsDone.Services/Contracts/IWorkersAsyncService.cs
@@ -1,8 +1,10 @@
-﻿using WhenItsDone.Models;
+﻿using System.Collections.Generic;
+using WhenItsDone.Models;
 
 namespace WhenItsDone.Services.Contracts
 {
     public interface IWorkersAsyncService : IGenericAsyncService<Worker>
     {
+        IEnumerable<Worker> GetWorkersWithDIshes();
     }
 }
