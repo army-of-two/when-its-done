f48498fc61ff4989acb3f4169255347f3fdba4b3
shakuu <todorov.h@gmail.com>
2017-02-01 14:05:31 +0200

    updated user model with required props

--- WhenItsDone/Lib/WhenItsDone.Models/User.cs
+++ WhenItsDone/Lib/WhenItsDone.Models/User.cs
@@ -7,6 +7,16 @@ namespace WhenItsDone.Models
         // TODO: 
         public int Id { get; set; }
 
+        public string Username { get; set; }
+
+        public int ClientId { get; set; }
+
+        public virtual Client Client { get; set; }
+
+        public int WorkerId { get; set; }
+
+        public virtual Worker Worker { get; set; }
+
         public bool IsDeleted { get; set; }
     }
 }
