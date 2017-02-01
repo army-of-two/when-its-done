2b9e9793144cfbf4d6fe512651e978f6522d1ad6
Stoiko Neykov <stoikoneykov@gmail.com>
2017-02-01 15:16:21 +0200

    added WorkerWithDIshesView

---
+++ WhenItsDone/Lib/WhenItsDone.DTOs/WorkerVIewsDTOs/WorkerWithDishesDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using WhenItsDone.Models;
+
+namespace WhenItsDone.DTOs.WorkerVIewsDTOs
+{
+    public class WorkerWithDishesDTO
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public IEnumerable<Dish> Dishes { get; set; }
+    }
+}
