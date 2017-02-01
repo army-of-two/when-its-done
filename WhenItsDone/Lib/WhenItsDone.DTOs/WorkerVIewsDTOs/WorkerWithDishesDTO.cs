04708b7e860671cf08058ea328f237b337bdd809
Stoiko Neykov <stoikoneykov@gmail.com>
2017-02-01 15:28:22 +0200

    update WorkerWithDishesDTO to huge model

--- WhenItsDone/Lib/WhenItsDone.DTOs/WorkerVIewsDTOs/WorkerWithDishesDTO.cs
+++ WhenItsDone/Lib/WhenItsDone.DTOs/WorkerVIewsDTOs/WorkerWithDishesDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using WhenItsDone.Models;
+using WhenItsDone.Models.Enums;
 
 namespace WhenItsDone.DTOs.WorkerVIewsDTOs
 {
@@ -10,6 +11,22 @@ namespace WhenItsDone.DTOs.WorkerVIewsDTOs
         public string FirstName { get; set; }
 
         public string LastName { get; set; }
+
+        public GenderType Gender { get; set; }
+
+        public int Age { get; set; }
+
+        public int Rating { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Country { get; set; }
+
+        public string City { get; set; }
+
+        public string Street { get; set; }
 
         public IEnumerable<Dish> Dishes { get; set; }
     }
