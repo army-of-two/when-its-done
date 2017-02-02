282bfdc4ecfc35b3e66dfb8613011f29cf6caa51
Stoiko Neykov <stoikoneykov@gmail.com>
2017-02-02 11:34:21 +0200

    added constructor_should Throw_ArgumentNulLException_WithProperMessage_When_Service_IsNull test

---
+++ WhenItsDone/Tests/LibTests/WhenItsDone.MVP.Tests/AdminPageControlsTests/APWorkersControlMVP/APWorkersControlPresenterTests/Constructor_Should.cs
@@ -0,0 +1,19 @@
+﻿using Moq;
+using NUnit.Framework;
+using WhenItsDone.MVP.AdminPageControls.APWorkersControlMVP;
+
+namespace WhenItsDone.MVP.Tests.AdminPageControlsTests.APWorkersControlMVP.APWorkersControlPresenterTests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+        [Test]
+        public void Throw_ArgumentNulLException_WithProperMessage_When_Service_IsNull()
+        {
+            var mockedView = new Mock<IAPWorkersControlView>();
+            
+            Assert.That(() => new APWorkersControlPresenter(mockedView.Object, null),
+                            Throws.ArgumentNullException.With.Message.Contains("WorkersService"));
+        }
+    }
+}
