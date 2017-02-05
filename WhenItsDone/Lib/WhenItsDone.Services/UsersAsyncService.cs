c9747bc2a2ad4e29cdda0df1389ac1aa8fcc16a2
shakuu <todorov.h@gmail.com>
2017-02-05 17:56:07 +0200

    added UsersRegistrationAsyncService

--- WhenItsDone/Lib/WhenItsDone.Services/UsersAsyncService.cs
+++ WhenItsDone/Lib/WhenItsDone.Services/UsersAsyncService.cs
@@ -3,7 +3,6 @@
 using WhenItsDone.Data.Contracts;
 using WhenItsDone.Data.UnitsOfWork.Factories;
 using WhenItsDone.Models;
-using WhenItsDone.Models.Factories;
 using WhenItsDone.Services.Abstraction;
 using WhenItsDone.Services.Contracts;
 
@@ -12,40 +11,13 @@ namespace WhenItsDone.Services
     public class UsersAsyncService : GenericAsyncService<User>, IUsersAsyncService, IGenericAsyncService<User>
     {
         private readonly IUsersAsyncRepository asyncRepository;
-        private readonly IInitializedUserFactory userDbModelFactory;
 
-        public UsersAsyncService(IUsersAsyncRepository asyncRepository, IDisposableUnitOfWorkFactory unitOfWorkFactory, IInitializedUserFactory userDbModelFactory)
+        public UsersAsyncService(IUsersAsyncRepository asyncRepository, IDisposableUnitOfWorkFactory unitOfWorkFactory)
             : base(asyncRepository, unitOfWorkFactory)
         {
-            Guard.WhenArgument(userDbModelFactory, nameof(IInitializedUserFactory)).IsNull();
             Guard.WhenArgument(asyncRepository, nameof(IUsersAsyncRepository)).IsNull();
 
-            this.userDbModelFactory = userDbModelFactory;
             this.asyncRepository = asyncRepository;
-        }
-
-        public bool CreateUser(string username)
-        {
-            var isSuccessful = false;
-
-            if (string.IsNullOrEmpty(username))
-            {
-                return isSuccessful;
-            }
-
-            var nextUser = this.userDbModelFactory.GetInitializedUser(username);
-
-            this.asyncRepository.Add(nextUser);
-            using (var unitOfWork = base.UnitOfWorkFactory.CreateUnitOfWork())
-            {
-                var result = unitOfWork.SaveChanges();
-                if (result != 0)
-                {
-                    isSuccessful = true;
-                }
-            }
-
-            return isSuccessful;
         }
     }
 }
