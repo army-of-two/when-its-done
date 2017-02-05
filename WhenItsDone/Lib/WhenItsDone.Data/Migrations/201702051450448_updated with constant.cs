a009d1c4d2e354db21467a57d648307a576400ec
shakuu <todorov.h@gmail.com>
2017-02-05 16:52:18 +0200

    updating database

---
+++ WhenItsDone/Lib/WhenItsDone.Data/Migrations/201702051450448_updated with constant.cs
@@ -0,0 +1,16 @@
+namespace WhenItsDone.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class updatedwithconstant : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
