1027ae744083745f98925ca9263dd4d35a97afe0
shakuu <todorov.h@gmail.com>
2017-02-05 15:53:28 +0200

    added profile picture model

---
+++ WhenItsDone/Lib/WhenItsDone.Models/ProfilePicture.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WhenItsDone.Models
+{
+    public class ProfilePicture
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string PictureBase64 { get; set; }
+    }
+}
