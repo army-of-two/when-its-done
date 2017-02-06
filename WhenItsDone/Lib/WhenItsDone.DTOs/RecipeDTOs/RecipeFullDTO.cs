aa5729c6a9cad966c0b18d756600801356792b73
Stoiko Neykov <stoikoneykov@gmail.com>
2017-02-06 11:58:32 +0200

    added DishWithRecipeAndPhotosDTO and RecipeFullDTO

---
+++ WhenItsDone/Lib/WhenItsDone.DTOs/RecipeDTOs/RecipeFullDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using WhenItsDone.DTOs.IngredientDTOs;
+
+namespace WhenItsDone.DTOs.RecipeDTOs
+{
+    public class RecipeFullDTO
+    {
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+        
+        public string Description { get; set; }
+
+        public virtual ICollection<IngredientWithFoodIdAndNameDTO> Ingredients { get; set; }
+
+        public bool IsDeleted { get; set; }
+    }
+}
