4f52acbfde63115cf6f63c83ef1bc5769bdc9a7c
Stoiko Neykov <stoikoneykov@gmail.com>
2017-02-06 11:14:23 +0200

    added AddressFullDTO

---
+++ WhenItsDone/Lib/WhenItsDone.DTOs/AddressDTOs/AddressFullDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace WhenItsDone.DTOs.AddressDTOs
+{
+    public class AddressFullDTO
+    {
+        public int Id { get; set; }
+        
+        public string Country { get; set; }
+        
+        public string City { get; set; }
+        
+        public string Street { get; set; }
+    }
+}
