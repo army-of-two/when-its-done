c0d421f44fcd74c5b004945045fb6d4132997e23
Stoiko Neykov <stoikoneykov@gmail.com>
2017-02-06 11:16:54 +0200

    added ContactInformationFullWithNestedDTO

---
+++ WhenItsDone/Lib/WhenItsDone.DTOs/ContactInformationDTOs/ContactInformationFullWithNestedDTO.cs
@@ -0,0 +1,15 @@
+﻿using WhenItsDone.DTOs.AddressDTOs;
+
+namespace WhenItsDone.DTOs.ContactInformationDTOs
+{
+    public class ContactInformationFullWithNestedDTO
+    {
+        public int Id { get; set; }
+
+        public virtual AddressFullDTO Address { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+    }
+}
