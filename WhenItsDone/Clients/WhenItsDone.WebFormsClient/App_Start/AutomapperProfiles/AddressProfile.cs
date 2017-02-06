33f20739bc94802d60865cdc453da7e163c244d3
Stoiko Neykov <stoikoneykov@gmail.com>
2017-02-06 12:31:05 +0200

    added ContactInformationProfile

--- WhenItsDone/Clients/WhenItsDone.WebFormsClient/App_Start/AutomapperProfiles/AddressProfile.cs
+++ WhenItsDone/Clients/WhenItsDone.WebFormsClient/App_Start/AutomapperProfiles/AddressProfile.cs
@@ -9,7 +9,7 @@ namespace WhenItsDone.WebFormsClient.App_Start.AutomapperProfiles
         protected AddressProfile(string profileName)
             : base(profileName)
         {
-            this.CreateMap<AddressFullDTO, Address>()
+            this.CreateMap<Address, AddressFullDTO>()
                 .ForMember(dest => dest.Id, opts => opts.MapFrom(x => x.Id))
                 .ForMember(dest => dest.Country, opts => opts.MapFrom(x => x.Country))
                 .ForMember(dest => dest.Street, opts => opts.MapFrom(x => x.Street))
---
+++ WhenItsDone/Clients/WhenItsDone.WebFormsClient/App_Start/AutomapperProfiles/ContactInformationProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using WhenItsDone.DTOs.ContactInformationDTOs;
+using WhenItsDone.Models;
+
+namespace WhenItsDone.WebFormsClient.App_Start.AutomapperProfiles
+{
+    public class ContactInformationProfile : Profile
+    {
+        protected ContactInformationProfile(string profileName)
+            : base(profileName)
+        {
+            this.CreateMap<ContactInformation, ContactInformationFullWithNestedDTO>()
+                .ForMember(dest => dest.Id, opts => opts.MapFrom(x => x.Id))
+                .ForMember(dest => dest.Email, opts => opts.MapFrom(x => x.Email))
+                .ForMember(dest => dest.PhoneNumber, opts => opts.MapFrom(x => x.PhoneNumber))
+                .ForMember(dest => dest.Address, opts => opts.MapFrom(x => x.Address));
+        }
+    }
+}
