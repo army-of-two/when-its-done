f638d0e8a5e23b409a25484dc1e3609eb038d7e9
shakuu <todorov.h@gmail.com>
2017-02-06 15:52:44 +0200

    updated VideoItem Model

--- WhenItsDone/Lib/WhenItsDone.Models/VideoItem.cs
+++ WhenItsDone/Lib/WhenItsDone.Models/VideoItem.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+
 using WhenItsDone.Models.Constants;
 using WhenItsDone.Models.Contracts;
 
@@ -13,6 +14,8 @@ namespace WhenItsDone.Models
         [MinLength(ValidationConstants.UrlLengthMinLength)]
         [MaxLength(ValidationConstants.UrlLengthMaxValue)]
         public string Url { get; set; }
+
+        public int Rating { get; set; }
 
         public bool IsDeleted { get; set; }
     }
