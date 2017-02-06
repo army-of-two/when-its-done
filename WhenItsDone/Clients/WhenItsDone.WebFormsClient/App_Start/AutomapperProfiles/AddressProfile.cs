e10b23013afbb4e604018886164667c567bac2ef
Stoiko Neykov <stoikoneykov@gmail.com>
2017-02-06 12:23:52 +0200

    added AddressProfile

---
+++ WhenItsDone/Clients/WhenItsDone.WebFormsClient/App_Start/AutomapperProfiles/AddressProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using WhenItsDone.DTOs.AddressDTOs;
+using WhenItsDone.Models;
+
+namespace WhenItsDone.WebFormsClient.App_Start.AutomapperProfiles
+{
+    public class AddressProfile : Profile
+    {
+        protected AddressProfile(string profileName)
+            : base(profileName)
+        {
+            this.CreateMap<AddressFullDTO, Address>()
+                .ForMember(dest => dest.Id, opts => opts.MapFrom(x => x.Id))
+                .ForMember(dest => dest.Country, opts => opts.MapFrom(x => x.Country))
+                .ForMember(dest => dest.Street, opts => opts.MapFrom(x => x.Street))
+                .ForMember(dest => dest.City, opts => opts.MapFrom(x => x.City));
+        }
+    }
+}
