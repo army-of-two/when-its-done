093c7b83590fa45baa53cf66603e8ae8d4673849
Stoiko Neykov <stoikoneykov@gmail.com>
2017-02-06 11:34:10 +0200

    added PhotoItemDTO

---
+++ WhenItsDone/Lib/WhenItsDone.DTOs/PhotoItemDTOs/PhotoItemDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace WhenItsDone.DTOs.PhotoItemDTOs
+{
+    public class PhotoItemDTO
+    {
+        public int Id { get; set; }
+
+        public string Url { get; set; }
+
+        public bool IsDeleted { get; set; }
+    }
+}
