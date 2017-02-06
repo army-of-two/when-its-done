382bb1c7915896fefa8c91fef0a6eedc726a2c41
Stoiko Neykov <stoikoneykov@gmail.com>
2017-02-06 11:39:30 +0200

    added IngredientDTOs

---
+++ WhenItsDone/Lib/WhenItsDone.DTOs/IngredientDTOs/IngredientWithFoodIdAndNameDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace WhenItsDone.DTOs.IngredientDTOs
+{
+    public class IngredientWithFoodIdAndNameDTO
+    {
+        public int Id { get; set; }
+
+        public int FoodId { get; set; }
+
+        public string Food { get; set; }
+
+        public decimal Quantity { get; set; }
+
+        public bool IsDeleted { get; set; }
+    }
+}
