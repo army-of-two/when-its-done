47931172fee5449bb48fbd28b16c0a5c9747b62a
shakuu <todorov.h@gmail.com>
2017-02-06 12:54:53 +0200

    updated DishViewsProfile

--- WhenItsDone/Clients/WhenItsDone.WebFormsClient/App_Start/AutomapperProfiles/DishViewsProfile.cs
+++ WhenItsDone/Clients/WhenItsDone.WebFormsClient/App_Start/AutomapperProfiles/DishViewsProfile.cs
@@ -15,6 +15,12 @@ namespace WhenItsDone.WebFormsClient.App_Start.AutomapperProfiles
                 .ForMember(dest => dest.Id, opts => opts.MapFrom(src => src.Id))
                 .ForMember(dest => dest.Name, opts => opts.MapFrom(src => src.Recipe.Name))
                 .ForMember(dest => dest.PhotoItemUrl, opts => opts.MapFrom(src => src.PhotoItems.FirstOrDefault().Url));
+
+            this.CreateMap<Dish, NamePhotoRatingDishViewDTO>()
+                .ForMember(dest => dest.Id, opts => opts.MapFrom(src => src.Id))
+                .ForMember(dest => dest.Name, opts => opts.MapFrom(src => src.Recipe.Name))
+                .ForMember(dest => dest.PhotoItemUrl, opts => opts.MapFrom(src => src.PhotoItems.FirstOrDefault().Url))
+                .ForMember(dest => dest.Rating, opts => opts.MapFrom(src => src.Rating));
         }
     }
 }
