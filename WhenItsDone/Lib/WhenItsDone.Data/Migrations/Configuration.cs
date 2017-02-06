871195b9559c1c72b88b8588d61788ca781544c9
shakuu <todorov.h@gmail.com>
2017-02-06 20:12:39 +0200

    updated seed method

--- WhenItsDone/Lib/WhenItsDone.Data/Migrations/Configuration.cs
+++ WhenItsDone/Lib/WhenItsDone.Data/Migrations/Configuration.cs
@@ -49,26 +49,47 @@ namespace WhenItsDone.Data.Migrations
 
             if (!context.VideoItems.Any())
             {
-                var mozart = new VideoItem();
-                mozart.Title = "Mozart";
-                mozart.YouTubeUrl = "https://www.youtube.com/watch?v=Rb0UmrCXxVA";
-                mozart.YouTubeId = "Rb0UmrCXxVA";
-                mozart.Rating = 100;
-                context.VideoItems.Add(mozart);
+                var potatoStacks = new VideoItem();
+                potatoStacks.Title = "Potato Stacks";
+                potatoStacks.YouTubeUrl = "https://www.youtube.com/watch?v=OLUkj1At-GQ";
+                potatoStacks.YouTubeId = "OLUkj1At-GQ";
+                potatoStacks.Rating = 100;
+                context.VideoItems.Add(potatoStacks);
 
-                var minimal = new VideoItem();
-                minimal.Title = "Minimal";
-                minimal.YouTubeUrl = "https://www.youtube.com/watch?v=_3P4j6iIVew";
-                minimal.YouTubeId = "_3P4j6iIVew";
-                minimal.Rating = 100;
-                context.VideoItems.Add(minimal);
+                var schrimpScampi = new VideoItem();
+                schrimpScampi.Title = "Shrimp Scampi";
+                schrimpScampi.YouTubeUrl = "https://www.youtube.com/watch?v=NLzqkhpniro";
+                schrimpScampi.YouTubeId = "NLzqkhpniro";
+                schrimpScampi.Rating = 100;
+                context.VideoItems.Add(schrimpScampi);
 
-                var metallica = new VideoItem();
-                metallica.Title = "Metallica";
-                metallica.YouTubeUrl = "https://www.youtube.com/watch?v=md3B3I7Nmvw";
-                metallica.YouTubeId = "md3B3I7Nmvw";
-                metallica.Rating = 100;
-                context.VideoItems.Add(metallica);
+                var ribEye = new VideoItem();
+                ribEye.Title = "Rib Eye Steak";
+                ribEye.YouTubeUrl = "https://www.youtube.com/watch?v=723K_WxpQe8";
+                ribEye.YouTubeId = "723K_WxpQe8";
+                ribEye.Rating = 100;
+                context.VideoItems.Add(ribEye);
+
+                //var mozart = new VideoItem();
+                //mozart.Title = "Mozart";
+                //mozart.YouTubeUrl = "https://www.youtube.com/watch?v=Rb0UmrCXxVA";
+                //mozart.YouTubeId = "Rb0UmrCXxVA";
+                //mozart.Rating = 100;
+                //context.VideoItems.Add(mozart);
+
+                //var minimal = new VideoItem();
+                //minimal.Title = "Minimal";
+                //minimal.YouTubeUrl = "https://www.youtube.com/watch?v=_3P4j6iIVew";
+                //minimal.YouTubeId = "_3P4j6iIVew";
+                //minimal.Rating = 100;
+                //context.VideoItems.Add(minimal);
+
+                //var metallica = new VideoItem();
+                //metallica.Title = "Metallica";
+                //metallica.YouTubeUrl = "https://www.youtube.com/watch?v=md3B3I7Nmvw";
+                //metallica.YouTubeId = "md3B3I7Nmvw";
+                //metallica.Rating = 100;
+                //context.VideoItems.Add(metallica);
 
                 context.SaveChanges();
             }
