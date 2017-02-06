8612d103e82d7f2e7af654bdbf9a433d0a7d2a43
Stoiko Neykov <stoikoneykov@gmail.com>
2017-02-06 12:54:46 +0200

    added MedicalInformationProfile

---
+++ WhenItsDone/Clients/WhenItsDone.WebFormsClient/App_Start/AutomapperProfiles/MedicalInformationProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using WhenItsDone.DTOs.MedicalInformationDTOs;
+using WhenItsDone.Models;
+
+namespace WhenItsDone.WebFormsClient.App_Start.AutomapperProfiles
+{
+    public class MedicalInformationProfile : Profile
+    {
+        protected MedicalInformationProfile(string profileName)
+            : base(profileName)
+        {
+            this.CreateMap<MedicalInformation, MedicalInformationFullDTO>()
+                .ForMember(dest => dest.Id, opts => opts.MapFrom(x => x.Id))
+                .ForMember(dest => dest.BMI, opts => opts.MapFrom(x => x.BMI))
+                .ForMember(dest => dest.BustSizeInCm, opts => opts.MapFrom(x => x.BustSizeInCm))
+                .ForMember(dest => dest.HeightInCm, opts => opts.MapFrom(x => x.HeightInCm))
+                .ForMember(dest => dest.HipSizeInCm, opts => opts.MapFrom(x => x.HipSizeInCm))
+                .ForMember(dest => dest.WaistSizeInCm, opts => opts.MapFrom(x => x.WaistSizeInCm))
+                .ForMember(dest => dest.WeightInKg, opts => opts.MapFrom(x => x.WeightInKg));
+        }
+    }
+}
