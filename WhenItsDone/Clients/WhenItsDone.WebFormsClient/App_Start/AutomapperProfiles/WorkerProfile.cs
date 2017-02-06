ca2806a5ba92ee87dfb5c6852335242fae45cdae
Stoiko Neykov <stoikoneykov@gmail.com>
2017-02-06 16:25:13 +0200

    added WorkerDetailInformationDTO

--- WhenItsDone/Clients/WhenItsDone.WebFormsClient/App_Start/AutomapperProfiles/WorkerProfile.cs
+++ WhenItsDone/Clients/WhenItsDone.WebFormsClient/App_Start/AutomapperProfiles/WorkerProfile.cs
@@ -14,6 +14,19 @@ namespace WhenItsDone.WebFormsClient.App_Start.AutomapperProfiles
                 .ForMember(dest => dest.FirstName, opts => opts.MapFrom(x => x.FirstName))
                 .ForMember(dest => dest.LastName, opts => opts.MapFrom(x => x.LastName));
 
+            this.CreateMap<Worker, WorkerDetailInformationDTO>()
+                .ForMember(dest => dest.Id, opts => opts.MapFrom(x => x.Id))
+                .ForMember(dest => dest.AddressInformationId, opts => opts.MapFrom(x => x.ContactInformationId))
+                .ForMember(dest => dest.Age, opts => opts.MapFrom(x => x.Age))
+                .ForMember(dest => dest.City, opts => opts.MapFrom(x => x.ContactInformation.Address.City))
+                .ForMember(dest => dest.ContactInformationId, opts => opts.MapFrom(x => x.ContactInformationId))
+                .ForMember(dest => dest.Country, opts => opts.MapFrom(x => x.ContactInformation.Address.Country))
+                .ForMember(dest => dest.Email, opts => opts.MapFrom(x => x.ContactInformation.Email))
+                .ForMember(dest => dest.Gender, opts => opts.MapFrom(x => x.Gender))
+                .ForMember(dest => dest.PhoneNumber, opts => opts.MapFrom(x => x.ContactInformation.PhoneNumber))
+                .ForMember(dest => dest.Rating, opts => opts.MapFrom(x => x.Rating))
+                .ForMember(dest => dest.Street, opts => opts.MapFrom(x => x.ContactInformation.Address.Street));
+
             this.CreateMap<Worker, WorkerWithDishesDTO>()
                 .ForMember(dest => dest.Id, opts => opts.MapFrom(x => x.Id))
                 .ForMember(dest => dest.Age, opts => opts.MapFrom(x => x.Age))
