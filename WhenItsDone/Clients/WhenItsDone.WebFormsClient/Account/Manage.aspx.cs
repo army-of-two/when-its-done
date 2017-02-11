941bc2383bbf6997a12849e2891a338cd77f5a77
shakuu <todorov.h@gmail.com>
2017-02-12 00:31:54 +0200

    updated Manage

--- WhenItsDone/Clients/WhenItsDone.WebFormsClient/Account/Manage.aspx.cs
+++ WhenItsDone/Clients/WhenItsDone.WebFormsClient/Account/Manage.aspx.cs
@@ -1,10 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Web.UI;
 
 namespace WhenItsDone.WebFormsClient.Account
 {
     public partial class Manage : System.Web.UI.Page
     {
+        private const string ManageProfilePictureUserControlId = "ManageProfilePictureUserControl";
+        private const string UpdatePersonalInformationUserControlId = "UpdatePersonalInformationUserControl";
+        private const string UpdateMedicalInformationUserControlId = "UpdateMedicalInformationUserControl";
+        private const string UpdateContactInformationUserControlId = "UpdateContactInformationUserControl";
+
+        private IDictionary<string, Control> manageControls;
+
         protected override void OnLoad(EventArgs e)
         {
             base.OnLoad(e);
@@ -12,38 +20,59 @@ namespace WhenItsDone.WebFormsClient.Account
             if (!this.IsPostBack)
             {
                 this.ProfilePictureButtonClick(null, null);
+                this.manageControls = this.BuildManageControlsDictionary();
             }
         }
 
         protected void ProfilePictureButtonClick(object sender, EventArgs e)
         {
             this.HideAllActivePanelControls();
-            this.ManageProfilePictureUserControl.Visible = true;
+            this.ActiveContent.Controls.Add(this.manageControls[Manage.ManageProfilePictureUserControlId]);
         }
 
         protected void PersonalInformationButtonClick(object sender, EventArgs e)
         {
             this.HideAllActivePanelControls();
-            this.UpdatePersonalInformationUserControl.Visible = true;
+            this.ActiveContent.Controls.Add(this.manageControls[Manage.UpdatePersonalInformationUserControlId]);
         }
 
         protected void MedicalInformationButtonClick(object sender, EventArgs e)
         {
             this.HideAllActivePanelControls();
-            this.UpdateMedicalInformationUserControl.Visible = true;
+            this.ActiveContent.Controls.Add(this.manageControls[Manage.UpdateMedicalInformationUserControlId]);
         }
 
         protected void ContactInformationButtonClick(object sender, EventArgs e)
         {
             this.HideAllActivePanelControls();
-            this.UpdateContactInformationUserControl.Visible = true;
+            this.ActiveContent.Controls.Add(this.manageControls[Manage.UpdateContactInformationUserControlId]);
+        }
+
+        private IDictionary<string, Control> BuildManageControlsDictionary()
+        {
+            var manageControls = new Dictionary<string, Control>();
+
+            var controlsCount = this.ActiveContent.Controls.Count;
+            for (int i = controlsCount - 1; i >= 0; i--)
+            {
+                Control control = this.ActiveContent.Controls[i];
+                if (control.ID.Contains("UserControl"))
+                {
+                    manageControls.Add(control.ID, control);
+                }
+
+                this.ActiveContent.Controls.RemoveAt(i);
+            }
+
+            return manageControls;
         }
 
         private void HideAllActivePanelControls()
         {
-            foreach (Control item in this.ActiveContent.Controls)
+            var controlsCount = this.ActiveContent.Controls.Count;
+            for (int i = controlsCount - 1; i >= 0; i--)
             {
-                item.Visible = false;
+                this.ActiveContent.Controls.RemoveAt(i);
             }
         }
     }
