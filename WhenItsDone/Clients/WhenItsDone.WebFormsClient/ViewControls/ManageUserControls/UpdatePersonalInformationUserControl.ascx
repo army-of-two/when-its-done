644f38908d832ebdb5436c6d04740c5352e16d7e
shakuu <todorov.h@gmail.com>
2017-02-11 09:54:45 +0200

    updated UpdatePersonalInformationUserControl

--- WhenItsDone/Clients/WhenItsDone.WebFormsClient/ViewControls/ManageUserControls/UpdatePersonalInformationUserControl.ascx
+++ WhenItsDone/Clients/WhenItsDone.WebFormsClient/ViewControls/ManageUserControls/UpdatePersonalInformationUserControl.ascx
@@ -23,3 +23,15 @@
             DefaultValue="" />
     </WhereParameters>
 </asp:EntityDataSource>
+
+<asp:GridView
+    ID="UserPersonalInfomationGridView" runat="server"
+    DataSourceID="UsersDataSource"
+    AutoGenerateColumns="false">
+    <Columns>
+        <asp:CommandField ShowEditButton="true" />
+        <asp:BoundField DataField="FirstName" HeaderText="First Name" />
+        <asp:BoundField DataField="LastName" HeaderText="Last Name" />
+        <asp:BoundField DataField="Age" HeaderText="Age" />
+    </Columns>
+</asp:GridView>
--- WhenItsDone/Clients/WhenItsDone.WebFormsClient/ViewControls/ManageUserControls/UpdatePersonalInformationUserControl.ascx.designer.cs
+++ WhenItsDone/Clients/WhenItsDone.WebFormsClient/ViewControls/ManageUserControls/UpdatePersonalInformationUserControl.ascx.designer.cs
@@ -29,5 +29,14 @@ namespace WhenItsDone.WebFormsClient.ViewControls.ManageUserControls {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.EntityDataSource UsersDataSource;
+        
+        /// <summary>
+        /// UserPersonalInfomationGridView control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView UserPersonalInfomationGridView;
     }
 }
