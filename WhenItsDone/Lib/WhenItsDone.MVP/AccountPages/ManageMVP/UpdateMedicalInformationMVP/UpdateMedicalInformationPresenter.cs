34ff21e18dd5c0552f252dda5a54d249818a8aad
shakuu <todorov.h@gmail.com>
2017-02-11 16:00:42 +0200

    updated UpdateMedicalInformationPresenter with OnInitialState

--- WhenItsDone/Lib/WhenItsDone.MVP/AccountPages/ManageMVP/UpdateMedicalInformationMVP/UpdateMedicalInformationPresenter.cs
+++ WhenItsDone/Lib/WhenItsDone.MVP/AccountPages/ManageMVP/UpdateMedicalInformationMVP/UpdateMedicalInformationPresenter.cs
@@ -1,19 +1,32 @@
-﻿using WebFormsMvp;
+﻿using Bytes2you.Validation;
+
+using WebFormsMvp;
+
+using WhenItsDone.Services.Contracts;
 
 namespace WhenItsDone.MVP.AccountPages.ManageMVP.UpdateMedicalInformationMVP
 {
     public class UpdateMedicalInformationPresenter : Presenter<IUpdateMedicalInformationView>, IUpdateMedicalInformationPresenter
     {
-        public UpdateMedicalInformationPresenter(IUpdateMedicalInformationView view)
+        private readonly IUsersAsyncService usersService;
+
+        public UpdateMedicalInformationPresenter(IUpdateMedicalInformationView view, IUsersAsyncService usersService)
             : base(view)
         {
+            Guard.WhenArgument(usersService, nameof(IUsersAsyncService)).IsNull().Throw();
+
+            this.usersService = usersService;
+
             this.View.InitialState += this.OnInitialState;
             this.View.UpdateValues += this.OnUpdateValues;
         }
 
         public void OnInitialState(object sender, UpdateMedicalInformationInitialStateEventArgs args)
         {
+            Guard.WhenArgument(args, nameof(UpdateMedicalInformationInitialStateEventArgs)).IsNull().Throw();
+            Guard.WhenArgument(args.LoggedUserUsername, nameof(args.LoggedUserUsername)).IsNull().Throw();
 
+            var foundUserMedicalInformation = this.usersService.GetCurrentUserMedicalInformation(args.LoggedUserUsername);
         }
 
         public void OnUpdateValues(object sender, UpdateMedicalInformationUpdateValuesEventArgs args)
