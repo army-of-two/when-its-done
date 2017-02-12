9dfb33ef924347d75d33ba363d1936049029d431
shakuu <todorov.h@gmail.com>
2017-02-12 13:47:20 +0200

    refactoring UsersAsyncRepository to base.DbSet

--- WhenItsDone/Lib/WhenItsDone.Data/Repositories/UsersAsyncRepository.cs
+++ WhenItsDone/Lib/WhenItsDone.Data/Repositories/UsersAsyncRepository.cs
@@ -22,35 +22,35 @@ namespace WhenItsDone.Data.Repositories
         {
             Guard.WhenArgument(username, nameof(username)).IsNullOrEmpty().Throw();
 
-            return this.DbSet.Where(user => user.Username == username).ProjectToFirstOrDefault<UsernameProfilePictureUserViewDTO>();
+            return base.DbSet.Where(user => user.Username == username).ProjectToFirstOrDefault<UsernameProfilePictureUserViewDTO>();
         }
 
         public ContactInformationUserViewDTO GetCurrentUserContactInformation(string username)
         {
             Guard.WhenArgument(username, nameof(username)).IsNullOrEmpty().Throw();
 
-            return this.DbSet.Where(user => user.Username == username).ProjectToFirstOrDefault<ContactInformationUserViewDTO>();
+            return base.DbSet.Where(user => user.Username == username).ProjectToFirstOrDefault<ContactInformationUserViewDTO>();
         }
 
         public MedicalInformationUserViewDTO GetCurrentUserMedicalInformation(string username)
         {
             Guard.WhenArgument(username, nameof(username)).IsNullOrEmpty().Throw();
 
-            return this.DbSet.Where(user => user.Username == username).ProjectToFirstOrDefault<MedicalInformationUserViewDTO>();
+            return base.DbSet.Where(user => user.Username == username).ProjectToFirstOrDefault<MedicalInformationUserViewDTO>();
         }
 
         public User GetCurrentUserIncludingMedicalInformation(string username)
         {
             Guard.WhenArgument(username, nameof(username)).IsNullOrEmpty().Throw();
 
-            return this.DbSet.Include(user => user.MedicalInformation).FirstOrDefault(user => user.Username == username);
+            return base.DbSet.Include(user => user.MedicalInformation).FirstOrDefault(user => user.Username == username);
         }
 
         public User GetCurrentUserIncludingContactInformation(string username)
         {
             Guard.WhenArgument(username, nameof(username)).IsNullOrEmpty().Throw();
 
-            return this.DbSet.Include(user => user.ContactInformation).Include(user => user.ContactInformation.Address).FirstOrDefault(user => user.Username == username);
+            return base.DbSet.Include(user => user.ContactInformation).Include(user => user.ContactInformation.Address).FirstOrDefault(user => user.Username == username);
         }
 
     }
