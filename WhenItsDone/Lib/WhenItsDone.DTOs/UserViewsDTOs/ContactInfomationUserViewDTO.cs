072426461ae7f58197415026ec1e7ac40f627d03
shakuu <todorov.h@gmail.com>
2017-02-12 12:05:55 +0200

    added ContactInfomationUserViewDTO

---
+++ WhenItsDone/Lib/WhenItsDone.DTOs/UserViewsDTOs/ContactInfomationUserViewDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace WhenItsDone.DTOs.UserViewsDTOs
+{
+    public class ContactInfomationUserViewDTO
+    {
+        public string Country { get; set; }
+
+        public string City { get; set; }
+
+        public string Street { get; set; }
+    }
+}
