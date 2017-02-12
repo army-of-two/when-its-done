e1b51dfeb46a3793c85d7a1312c91b324dd2d6bb
shakuu <todorov.h@gmail.com>
2017-02-12 12:03:59 +0200

    added IUsersAsyncService param to UpdateContactInformationPresenter

--- WhenItsDone/Lib/WhenItsDone.MVP/AccountPages/ManageMVP/UpdateContactInformationMVP/UpdateContactInformationPresenter.cs
+++ WhenItsDone/Lib/WhenItsDone.MVP/AccountPages/ManageMVP/UpdateContactInformationMVP/UpdateContactInformationPresenter.cs
@@ -1,14 +1,23 @@
 ﻿using System;
 
+using Bytes2you.Validation;
+
 using WebFormsMvp;
+using WhenItsDone.Services.Contracts;
 
 namespace WhenItsDone.MVP.AccountPages.ManageMVP.UpdateContactInformationMVP
 {
     public class UpdateContactInformationPresenter : Presenter<IUpdateContactInformationView>, IUpdateContactInformationPresenter
     {
-        public UpdateContactInformationPresenter(IUpdateContactInformationView view)
+        private readonly IUsersAsyncService usersService;
+
+        public UpdateContactInformationPresenter(IUpdateContactInformationView view, IUsersAsyncService usersService)
             : base(view)
         {
+            Guard.WhenArgument(usersService, nameof(IUsersAsyncService)).IsNull().Throw();
+
+            this.usersService = usersService;
+
             this.View.UpdateContactInformationInitialState += this.OnUpdateContactInformationInitialState;
             this.View.UpdateContactInformationUpdateValues += this.OnUpdateContactInformationUpdateValues;
         }
