6eee8bacb000311614852f4ecace110837179751
Stoiko Neykov <stoikoneykov@gmail.com>
2017-02-15 10:23:49 +0200

    added WorkerAsyncRepository Constructor_Should

---
+++ WhenItsDone/Tests/LibTests/WhenItsDone.Data.Tests/RepositoriesTests/WorkerAsyncRepositoryTests/Constructor_Should.cs
@@ -0,0 +1,26 @@
+﻿using Moq;
+using NUnit.Framework;
+using System.Data.Entity;
+using WhenItsDone.Data.Contracts;
+using WhenItsDone.Data.Repositories;
+using WhenItsDone.Models;
+
+namespace WhenItsDone.Data.Tests.RepositoriesTests.WorkerAsyncRepositoryTests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+        [Test]
+        public void Create_Instance_When_ParametersAre_Valid()
+        {
+            var mockedSet = new Mock<DbSet<Worker>>();
+
+            var mockedContext = new Mock<IWhenItsDoneDbContext>();
+            mockedContext.Setup(x => x.Set<Worker>()).Returns(mockedSet.Object);
+
+            var obj = new WorkerAsyncRepository(mockedContext.Object);
+
+            Assert.IsInstanceOf<WorkerAsyncRepository>(obj);
+        }
+    }
+}
