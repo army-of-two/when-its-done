93b74655a4130e510418f71af0e21f0ad169cff5
shakuu <todorov.h@gmail.com>
2017-02-17 18:57:13 +0200

    added DishDetailsViewDTO mapping

--- WhenItsDone/Clients/WhenItsDone.WebFormsClient/App_Start/AutomapperProfiles/DishViewsProfile.cs
+++ WhenItsDone/Clients/WhenItsDone.WebFormsClient/App_Start/AutomapperProfiles/DishViewsProfile.cs
@@ -32,6 +32,15 @@ namespace WhenItsDone.WebFormsClient.App_Start.AutomapperProfiles
                 .ForMember(dest => dest.Name, opts => opts.MapFrom(src => src.Recipe.Name))
                 .ForMember(dest => dest.PhotoItemUrl, opts => opts.MapFrom(src => src.PhotoItems.FirstOrDefault().Url))
                 .ForMember(dest => dest.Rating, opts => opts.MapFrom(src => src.Rating));
+
+            this.CreateMap<Dish, DishDetailsViewDTO>()
+                .ForMember(dest => dest.Id, opts => opts.MapFrom(src => src.Id))
+                .ForMember(dest => dest.Name, opts => opts.MapFrom(src => src.Recipe.Name))
+                .ForMember(dest => dest.Price, opts => opts.MapFrom(src => src.Price))
+                .ForMember(dest => dest.Calories, opts => opts.MapFrom(src => src.Recipe.NutritionFacts.Calories))
+                .ForMember(dest => dest.Carbohydrates, opts => opts.MapFrom(src => src.Recipe.NutritionFacts.Carbohydrates))
+                .ForMember(dest => dest.Fats, opts => opts.MapFrom(src => src.Recipe.NutritionFacts.Fats))
+                .ForMember(dest => dest.Protein, opts => opts.MapFrom(src => src.Recipe.NutritionFacts.Protein));
         }
     }
 }
