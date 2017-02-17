57d2b0e4e8d9851f17769d303e66c92168f40c58
shakuu <todorov.h@gmail.com>
2017-02-17 22:41:21 +0200

    added DishRatingPresenter

---
+++ WhenItsDone/Lib/WhenItsDone.MVP/RatingsMVP/IDishRatingView.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using WebFormsMvp;
+
+namespace WhenItsDone.MVP.RatingsMVP
+{
+    public interface IDishRatingView : IView<DishRatingViewModel>
+    {
+        event EventHandler<DishRatingVoteEventArgs> OnLikeVote;
+
+        event EventHandler<DishRatingVoteEventArgs> OnDislikeVote;
+    }
+}
