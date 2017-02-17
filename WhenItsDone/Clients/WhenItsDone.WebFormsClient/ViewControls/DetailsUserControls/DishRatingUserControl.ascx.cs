1f25fc54bfc8704aeaf75a1751f7025f807bb0d1
shakuu <todorov.h@gmail.com>
2017-02-17 22:42:45 +0200

    updated DishRatingUserControl

--- WhenItsDone/Clients/WhenItsDone.WebFormsClient/ViewControls/DetailsUserControls/DishRatingUserControl.ascx.cs
+++ WhenItsDone/Clients/WhenItsDone.WebFormsClient/ViewControls/DetailsUserControls/DishRatingUserControl.ascx.cs
@@ -1,7 +1,18 @@
-﻿namespace WhenItsDone.WebFormsClient.ViewControls.DetailsUserControls
+﻿using System;
+
+using WebFormsMvp;
+using WebFormsMvp.Web;
+
+using WhenItsDone.MVP.RatingsMVP;
+
+namespace WhenItsDone.WebFormsClient.ViewControls.DetailsUserControls
 {
-    public partial class DishRatingUserControl : System.Web.UI.UserControl
+    [PresenterBinding(typeof(IDishRatingPresenter))]
+    public partial class DishRatingUserControl : MvpUserControl<DishRatingViewModel>, IDishRatingView
     {
+        public event EventHandler<DishRatingVoteEventArgs> OnLikeVote;
+        public event EventHandler<DishRatingVoteEventArgs> OnDislikeVote;
+
         public string DishRating { get; set; }
 
         public string DishId { get; set; }
