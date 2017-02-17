6276c8e33e862d2b28abb22e182f9f4c8f10504f
Stoiko Neykov <stoikoneykov@gmail.com>
2017-02-17 15:55:53 +0200

    added StringEventArgs

---
+++ WhenItsDone/Lib/WhenItsDone.MVP/AdminPageControls/EventArguments/StringEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WhenItsDone.MVP.AdminPageControls.EventArguments
+{
+    public class StringEventArgs : EventArgs
+    {
+        public StringEventArgs(string stringParameter)
+        {
+            this.StringParameter = stringParameter;
+        }
+
+        public string StringParameter { get; set; }
+    }
+}
