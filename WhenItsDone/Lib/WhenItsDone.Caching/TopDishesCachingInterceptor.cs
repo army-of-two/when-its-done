b4870d02ffc3bf5fcd7908eafebf6936df738b02
shakuu <todorov.h@gmail.com>
2017-02-21 10:50:15 +0200

    added interceptor

---
+++ WhenItsDone/Lib/WhenItsDone.Caching/TopDishesCachingInterceptor.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Web;
+
+using Bytes2you.Validation;
+
+using Ninject.Extensions.Interception;
+
+using WhenItsDone.Data.Contracts;
+
+namespace WhenItsDone.Caching
+{
+    public class TopDishesCachingInterceptor : IInterceptor
+    {
+        private const string CacheItemName = "TopDishes";
+
+        private readonly IDishesAsyncRepository dishesAsyncRepository;
+
+        private DateTime? lastUpdate;
+
+        public TopDishesCachingInterceptor(IDishesAsyncRepository dishesAsyncRepository)
+        {
+            Guard.WhenArgument(dishesAsyncRepository, nameof(IDishesAsyncRepository)).IsNull().Throw();
+
+            this.dishesAsyncRepository = dishesAsyncRepository;
+        }
+
+        public void Intercept(IInvocation invocation)
+        {
+            var currentCachedContent = HttpContext.Current.Cache[TopDishesCachingInterceptor.CacheItemName];
+            if (currentCachedContent != null)
+            {
+                invocation.ReturnValue = currentCachedContent;
+                return;
+            }
+            else
+            {
+                var dishesCount = 3;
+                var addSampleData = true;
+
+                var topDishes = this.dishesAsyncRepository.GetTopCountDishesByRating(dishesCount).Result;
+                if (topDishes.Count < dishesCount && addSampleData == true)
+                {
+                    topDishes = this.dishesAsyncRepository.AddTopCountDishesSampleData(dishesCount, topDishes);
+                }
+
+                HttpContext.Current.Cache[TopDishesCachingInterceptor.CacheItemName] = topDishes;
+                invocation.ReturnValue = topDishes;
+                return;
+            }
+        }
+    }
+}
